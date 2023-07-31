78afa12968a873ec0a64aa2abca97e60c7e77504
yingjie_wang <2778809626@qq.com>
2023-07-31 09:39:25 +0800

    1. 添加接收一个二维矩阵，根据矩阵的阈值大小范围转换为不同颜色的点并在jpeg图片显示

--- su/image/jpeg_utils.cpp
+++ su/image/jpeg_utils.cpp
@@ -20,7 +20,7 @@ void my_error_exit(j_common_ptr cinfo)
 }
 
 
-bool jpeg_data::release()   
+bool jpeg_data::release()
 {
     if (nullptr == image_data)
     {
@@ -64,7 +64,7 @@ bool jpeg_utils::write_jpeg_data(const char* path, const jpeg_data& jpeg_data)
     jpeg_error_mgr jerr;
 
     JSAMPARRAY image_buffer = NULL;
-    JSAMPROW row_p[1];;
+    JSAMPROW row_p[1];
 
     int row, col;
 
@@ -75,7 +75,8 @@ bool jpeg_utils::write_jpeg_data(const char* path, const jpeg_data& jpeg_data)
     FILE* outfile;
     if ((outfile = fopen(path, "wb")) == NULL)
     {
-        ERREXIT(&cinfo, JERR_FILE_WRITE);
+        //ERREXIT(&cinfo, JERR_FILE_WRITE);
+        std::cout << "文件打开失败" << std::endl;
         return false;
     }
     jpeg_stdio_dest(&cinfo, outfile);
@@ -145,6 +146,13 @@ jpeg_data jpeg_utils::read_jpeg(const char* path)
 
     jpeg_start_decompress(&cinfo);
 
+    if (cinfo.out_color_space != JCS_RGB && cinfo.out_color_space != JCS_GRAYSCALE)
+    {
+        jpeg_finish_decompress(&cinfo);
+        jpeg_destroy_decompress(&cinfo);
+        fclose(infile);
+        return res_jpeg;
+    }
     row_stride = cinfo.output_width * cinfo.output_components;
 
     res_jpeg.jpeg_width = cinfo.output_width;
@@ -152,6 +160,7 @@ jpeg_data jpeg_utils::read_jpeg(const char* path)
     res_jpeg.jpeg_components = cinfo.output_components;
     res_jpeg.color_space = cinfo.out_color_space;
     res_jpeg.data_precision = cinfo.data_precision;
+
 
     buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr)&cinfo, JPOOL_IMAGE, row_stride, 1);
 
@@ -177,13 +186,13 @@ jpeg_data jpeg_utils::read_jpeg(const char* path)
 bool jpeg_data::set_pixel(const size_t& row, const size_t& col, const jpeg_pixel& pixel)
 {
 
-    if (row > jpeg_width || col > jpeg_height)
+    if (row >= jpeg_height || col >= jpeg_width)
     {
         std::cout << "Coordinates out of bounds " << std::endl;
         return false;
     }
-    int row_stride = jpeg_width * jpeg_components;
-    int start = (col * row_stride) + (row * jpeg_components);
+
+    int start = (col + row * jpeg_width) * jpeg_components;
     if (3 == jpeg_components)
     {
         image_data[start] = pixel.red;
@@ -191,12 +200,177 @@ bool jpeg_data::set_pixel(const size_t& row, const size_t& col, const jpeg_pixel
         image_data[start + 2] = pixel.blue;
 
     }
-    else if (1 == jpeg_components)
+    if (1 == jpeg_components)
     {
         image_data[start] = pixel.gray;
     }
 
     return true;
+}
+
+
+jpeg_data  jpeg_data::operator=(const jpeg_data& other)
+{
+    this->jpeg_width = other.jpeg_width;
+    this->jpeg_height = other.jpeg_height;
+    this->jpeg_components = other.jpeg_components;
+    this->color_space = other.color_space;
+    this->quality = other.quality;
+    this->data_precision = other.data_precision;
+    this->image_data = other.image_data;
+
+    return *this;
+
+}
+
+
+//bool jpeg_data::jpeg_gray_to_rgb(二维数组（）, vector<阈值>， vector《 jpeg_pixel》)
+bool jpeg_data::jpeg_gray_to_rgb()
+
+{
+#define LEVEL_0 0
+#define LEVEL_1 5
+#define LEVEL_2 15
+#define LEVEL_3 25
+#define LEVEL_4 50
+#define LEVEL_5 100
+#define LEVEL_6 255
+
+    //jpeg_pixel m_color_1(0xc9, 0xec, 0xc9);     //0-5
+    //jpeg_pixel m_color_2(0xc9, 0xec, 0xc9);     //5-15
+    //jpeg_pixel m_color_3(0xc9, 0xec, 0xc9);     //15-25     
+    //jpeg_pixel m_color_4(0xc9, 0xec, 0xc9);     //25-50
+    //jpeg_pixel m_color_5(0xc9, 0xec, 0xc9);     //50-100
+    //jpeg_pixel m_color_6(0xc9, 0xec, 0xc9);     //100-
+
+
+    char m_color_1[3] = { 0xc9, 0xec, 0xc9 };    //0-5
+    char m_color_2[3] = { 0x9b, 0xdd, 0xa9 };    //5-15
+    char m_color_3[3] = { 0x7e, 0xcc, 0x7b };    //15-2
+    char m_color_4[3] = { 0x89, 0x99, 0x00 };    //25-5
+    char m_color_5[3] = { 0x8f, 0x3a, 0x00 };    //50-1
+    char m_color_6[3] = { 0xFA, 0x00, 0xFA };    //100-
+
+    if (3 == jpeg_components)
+    {
+        std::cout << "image components = 3  " << std::endl;
+        return false;
+    }
+
+    if (nullptr == image_data)
+    {
+        std::cout << "image_data is empty " << std::endl;
+        return false;
+    }
+    jpeg_components = 3;
+    color_space = JCS_RGB;
+    unsigned char* temp_data = (unsigned char*)malloc(sizeof(unsigned char*) * jpeg_width * jpeg_height * jpeg_components);
+    int  jmp = sizeof(unsigned char) * jpeg_components;
+    unsigned char* temp_datai = temp_data;
+    for (int p = 0; p < jpeg_width * jpeg_height; p++)
+    {
+        char* dd = '\0\0\0';
+        // char dd[3] = { 0xc9, 0xec, 0xc9 };
+         //    memcpy(image_data + row * row_stride, buffer[0], row_stride);
+        if (image_data[p] >= LEVEL_0 && image_data[p] < LEVEL_1)
+        {
+            dd = m_color_1;
+        }
+        else if (image_data[p] >= LEVEL_1 && image_data[p] < LEVEL_2)
+        {
+            dd = m_color_2;
+        }
+        else if (image_data[p] >= LEVEL_2 && image_data[p] < LEVEL_3)
+        {
+            dd = m_color_3;
+        }
+        else if (image_data[p] >= LEVEL_3 && image_data[p] < LEVEL_4)
+        {
+            dd = m_color_4;
+        }
+        else //(image_data[p] >= LEVEL_5)
+        {
+            dd = m_color_5;
+        }
+        memcpy(temp_datai, dd, jmp);
+        temp_datai += jmp;
+    }
+
+
+    free(image_data);
+    image_data = temp_data;
+
+
+    return true;
+}
+
+bool jpeg_data::matrix2d_to_rgb_jpeg(UCMatrix matrix, std::vector<int> threshold, std::vector<char*> pixel_colors)
+{
+    if (threshold.size() < 6)
+    {
+        std::cout << "阈值不足6个 " << std::endl;
+        return false;
+    }
+    if (pixel_colors.size() < 5)
+    {
+        std::cout << "色彩不足5个 " << std::endl;
+        return false;
+    }
+    if (nullptr == matrix.data)
+    {
+        std::cout << "矩阵为空 " << std::endl;
+        return false;
+    }
+
+    this->jpeg_width = matrix.rows;
+    this->jpeg_height = matrix.cols;
+    this->jpeg_components = 3;
+    this->color_space = JCS_RGB;
+
+    this->image_data = (unsigned char*)malloc(sizeof(unsigned char*) * jpeg_width * jpeg_height * jpeg_components);
+    memset(this->image_data, 255, sizeof(unsigned char*) * jpeg_width * jpeg_height * jpeg_components);
+    int row_size = jpeg_width * jpeg_components;
+    int  jmp = sizeof(unsigned char) * jpeg_components;
+    for (int i = 0; i < jpeg_height; i++)
+    {
+        for (int j = 0; j < jpeg_width; j++)
+        {
+            //int p = i * row_size + j * jpeg_components;
+            //char* pp;
+            if (matrix.data[i][j] >= threshold[0] && matrix.data[i][j] < threshold[1])
+            {
+                set_pixel(i, j, jpeg_pixel(pixel_colors[0][0], pixel_colors[0][1], pixel_colors[0][2]));
+                //pp = pixel_colors[0];
+            }
+            else if (matrix.data[i][j] >= threshold[1] && matrix.data[i][j] < threshold[2])
+            {
+                set_pixel(i, j, jpeg_pixel(pixel_colors[1][0], pixel_colors[1][1], pixel_colors[1][2]));
+                //pp = pixel_colors[1];
+            }
+            else if (matrix.data[i][j] >= threshold[2] && matrix.data[i][j] < threshold[3])
+            {
+                set_pixel(i, j, jpeg_pixel(pixel_colors[2][0], pixel_colors[2][1], pixel_colors[2][2]));
+                //pp = pixel_colors[2];
+            }
+            else if (matrix.data[i][j] >= threshold[3] && matrix.data[i][j] < threshold[4])
+            {
+                set_pixel(i, j, jpeg_pixel(pixel_colors[3][0], pixel_colors[3][1], pixel_colors[3][2]));
+                //pp = pixel_colors[3];
+            }
+            else
+            {
+                set_pixel(i, j, jpeg_pixel(pixel_colors[4][0], pixel_colors[4][1], pixel_colors[4][2]));
+                //pp = pixel_colors[4];
+            }
+
+            //memcpy(image_data, pp, jmp);
+            //image_data += jmp;
+        }
+    }
+
+    return true;
+
+
 }
 
 
--- su/image/jpeg_utils.h
+++ su/image/jpeg_utils.h
@@ -15,14 +15,17 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "jpeglib.h"
 #include "jerror.h"
-#include <setjmp.h>    
-   
+#include <setjmp.h>
+
+#include "math\silly_matrix.h"
 
  // 像素颜色结构体 pixel_color
 struct jpeg_pixel
@@ -42,9 +45,17 @@ struct jpeg_pixel
 	JSAMPLE  red{ 0 };     // 红色分量
 	JSAMPLE  green{ 0 };   // 绿色分量
 	JSAMPLE  blue{ 0 };    // 蓝色分量
-	JSAMPLE  gray{ 0 };    // 蓝色分量
+	JSAMPLE  gray{ 0 };    // 灰色分量
 	// unsigned char = JSAMPLE
 };
+
+//template<typename T>
+//struct matrix_2d
+//{
+//	T** data;
+//	size_t rows;
+//	size_t cols;
+//};
 
 class jpeg_data
 {
@@ -56,14 +67,19 @@ public:
 
 	bool set_pixel(const size_t& row, const size_t& col, const jpeg_pixel& pixel);
 
+	bool jpeg_gray_to_rgb();
 
+	bool matrix2d_to_rgb_jpeg(UCMatrix matrix, std::vector<int> threshold, std::vector<char*> pixel_colors);
+
+
+	jpeg_data  operator=(const jpeg_data& other);
 	// 参数-----------------
 	JDIMENSION jpeg_width{ 0 };
 	JDIMENSION jpeg_height{ 0 };
 	int jpeg_components{ 3 };		// 色带通道数 1 ro 3
 	J_COLOR_SPACE color_space;		// 色彩空间必须为枚举常量之一，通常为JCS_RGB或JCS_GRAYSCALE
 	int quality{ 75 };
-	int data_precision;				/* 输入图像的数据精度 一般为8*/
+	int data_precision{ 8 };				/* 输入图像的数据精度 一般为8*/
 
 	unsigned char* image_data{ nullptr };
 
@@ -71,11 +87,12 @@ private:
 
 };
 
+
 class jpeg_utils
 {
 public:
 
-	jpeg_data creat_empty_jpeg(const size_t& width, const size_t& height, const size_t& components, const J_COLOR_SPACE& color_space, const size_t& quality = 75, const size_t& data_precision = 8);
+	static jpeg_data creat_empty_jpeg(const size_t& width, const size_t& height, const size_t& components, const J_COLOR_SPACE& color_space, const size_t& quality = 75, const size_t& data_precision = 8);
 
 
 	bool write_jpeg_data(const char* path, const jpeg_data& jpeg_data);
@@ -87,6 +104,9 @@ public:
 
 	jpeg_utils() = default;
 	~jpeg_utils() = default;
+
+private:
+
 };
 
 #endif //SILLY_UTILS_JPEG_UTILS_H
