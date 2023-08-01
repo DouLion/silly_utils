1dbe07cc7bb178678694dee76837e7eac7f08282
douliyang <734546831@qq.com>
2023-08-01 18:58:55 +0800

    其他项目调用时不完全编译

--- cmake/silly_utils.cmake
+++ cmake/silly_utils.cmake
@@ -9,9 +9,9 @@ set(SU_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 # file(MAKE_DIRECTORY ${SU_DIRECTORY})
 set(SOURCE_DIR "${SU_DIRECTORY}")
 set(NO_BUILD_SU_TEST True)
+set(SU_AS_SUB_PROJECT TRUE)
 set(SILLY_UTIL_TAG  master)  # 指定版本
 set(SILLY_UTILS_GIT_URL  http://123.56.193.136:16080/douliyang/silly_utils.git)  # 指定git仓库地址
-
 
 FetchContent_Declare(
   silly_utils
