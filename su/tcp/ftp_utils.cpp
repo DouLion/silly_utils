873f23214142827a2a4093e699764ebd631d72c5
douliyang <734546831@qq.com>
2023-08-01 16:57:48 +0800

    ftp文件上传状态不对

--- su/tcp/ftp_utils.cpp
+++ su/tcp/ftp_utils.cpp
@@ -243,87 +243,92 @@ bool ftp_utils::ftp_download(const std::string& remote_file_path, const std::str
 
 bool ftp_utils::ftp_upload(const std::string& localFileFath, const std::string& remote_file_path, const std::string& user, const std::string& passwd, int timeout, bool IsDir)
 {
-	{
-		CURL* curl;
-		CURLcode res;
-		FILE* hd_src;
-		struct stat file_info;
-		unsigned long fsize;
+	bool status = false;
+	CURL* curl;
+	CURLcode res;
+	FILE* hd_src;
+	struct stat file_info;
+	unsigned long fsize;
 
-		//struct curl_slist* headerlist = NULL;
-		/*static const char buf_1[] = "RNFR " UPLOAD_FILE_AS;
-		static const char buf_2[] = "RNTO " RENAME_FILE_TO;*/
+	//struct curl_slist* headerlist = NULL;
+	/*static const char buf_1[] = "RNFR " UPLOAD_FILE_AS;
+	static const char buf_2[] = "RNTO " RENAME_FILE_TO;*/
 
-		std::filesystem::path sp(localFileFath);
-		const char* file_name = sp.filename().string().c_str();
+	std::filesystem::path sp(localFileFath);
+	const char* file_name = sp.filename().string().c_str();
 
 
-		/* get the file size of the local file */
-		if (stat(localFileFath.c_str(), &file_info)) {
-			printf("Couldn't open '%s': %s\n", localFileFath.c_str(), strerror(errno));
-			return false;
-		}
-		fsize = (unsigned long)file_info.st_size;
-
-		printf("Local file size: %lu bytes.\n", fsize);
-
-		/* get a FILE * of the same file */
-		hd_src = fopen(localFileFath.c_str(), "rb");
-
-		/* In windows, this will init the winsock stuff */
-		curl_global_init(CURL_GLOBAL_ALL);
-
-		/* get a curl handle */
-		curl = curl_easy_init();
-		if (curl) {
-			/* build a list of commands to pass to libcurl */
-			/*headerlist = curl_slist_append(headerlist, file_name);
-			headerlist = curl_slist_append(headerlist, file_name);*/
-			std::string user_pwd = user + ":" + passwd;
-			//curl_easy_setopt(curl, CURLOPT_USERPWD, "bjtzxxxkjyxgs:Tzx1@3$");
-			curl_easy_setopt(curl, CURLOPT_USERPWD, user_pwd.c_str());
-
-			/* we want to use our own read function */
-			// curl_easy_setopt(curl, CURLOPT_READFUNCTION, ftp_upload_read_callback);
-
-			/* enable uploading */
-			curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-
-			/* specify target */
-			curl_easy_setopt(curl, CURLOPT_URL, remote_file_path.c_str());
-
-			/* pass in that last of FTP commands to run after the transfer */
-			//curl_easy_setopt(curl, CURLOPT_POSTQUOTE, headerlist);
-
-			/* now specify which file to upload */
-			curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
-
-			curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
-			curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, timeout);
-
-			/* Set the size of the file to upload (optional).  If you give a *_LARGE
-			   option you MUST make sure that the type of the passed-in argument is a
-			   curl_off_t. If you use CURLOPT_INFILESIZE (without _LARGE) you must
-			   make sure that to pass in a type 'long' argument. */
-			curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
-				(curl_off_t)fsize);
-
-			/* Now run off and do what you have been told! */
-			res = curl_easy_perform(curl);
-			/* Check for errors */
-			if (res != CURLE_OK)
-				fprintf(stderr, "curl_easy_perform() failed: %s\n",
-					curl_easy_strerror(res));
-
-			/* clean up the FTP commands list */
-			//curl_slist_free_all(headerlist);
-
-			/* always cleanup */
-			curl_easy_cleanup(curl);
-		}
-		fclose(hd_src); /* close the local file */
-
-		curl_global_cleanup();
-		return true;
+	/* get the file size of the local file */
+	if (stat(localFileFath.c_str(), &file_info)) {
+		printf("Couldn't open '%s': %s\n", localFileFath.c_str(), strerror(errno));
+		return status;
 	}
+	fsize = (unsigned long)file_info.st_size;
+
+	printf("Local file size: %lu bytes.\n", fsize);
+
+	/* get a FILE * of the same file */
+	hd_src = fopen(localFileFath.c_str(), "rb");
+
+	/* In windows, this will init the winsock stuff */
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	/* get a curl handle */
+	curl = curl_easy_init();
+	if (curl) {
+		/* build a list of commands to pass to libcurl */
+		/*headerlist = curl_slist_append(headerlist, file_name);
+		headerlist = curl_slist_append(headerlist, file_name);*/
+		std::string user_pwd = user + ":" + passwd;
+		//curl_easy_setopt(curl, CURLOPT_USERPWD, "bjtzxxxkjyxgs:Tzx1@3$");
+		curl_easy_setopt(curl, CURLOPT_USERPWD, user_pwd.c_str());
+
+		/* we want to use our own read function */
+		// curl_easy_setopt(curl, CURLOPT_READFUNCTION, ftp_upload_read_callback);
+
+		/* enable uploading */
+		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+
+		/* specify target */
+		curl_easy_setopt(curl, CURLOPT_URL, remote_file_path.c_str());
+
+		/* pass in that last of FTP commands to run after the transfer */
+		//curl_easy_setopt(curl, CURLOPT_POSTQUOTE, headerlist);
+
+		/* now specify which file to upload */
+		curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
+
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, timeout);
+
+		/* Set the size of the file to upload (optional).  If you give a *_LARGE
+		   option you MUST make sure that the type of the passed-in argument is a
+		   curl_off_t. If you use CURLOPT_INFILESIZE (without _LARGE) you must
+		   make sure that to pass in a type 'long' argument. */
+		curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
+			(curl_off_t)fsize);
+
+		/* Now run off and do what you have been told! */
+		res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK)
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
+		}
+		else
+		{
+			status = true;
+		}
+			
+		/* clean up the FTP commands list */
+		//curl_slist_free_all(headerlist);
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+	fclose(hd_src); /* close the local file */
+
+	curl_global_cleanup();
+	return status;
 }
