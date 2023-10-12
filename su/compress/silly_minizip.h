f59afd30704a7d6cffc366eaa9898895e32a7361
douliyang <734546831@qq.com>
2023-10-12 19:09:49 +0800

    1

--- su/compress/silly_minizip.h
+++ su/compress/silly_minizip.h
@@ -27,17 +27,16 @@
 
 #include "minizip/zip.h"
 #include "minizip/unzip.h"
-using namespace std;
 
 class silly_minizip
 {
 public:
-    static void EnumDirFiles(const string& dirPrefix, const string& dirName, vector<string>& vFiles)
+    static void EnumDirFiles(const std::string& dirPrefix, const std::string& dirName, std::vector<std::string>& vFiles)
     {
         if (dirPrefix.empty() || dirName.empty())
             return;
-        string dirNameTmp = dirName;
-        string dirPre = dirPrefix;
+        std::string dirNameTmp = dirName;
+        std::string dirPre = dirPrefix;
 
         if (dirNameTmp.find_last_of("/") != dirNameTmp.length() - 1)
             dirNameTmp += "/";
@@ -46,7 +45,7 @@ public:
         if (dirPre.find_last_of("/") != dirPre.length() - 1)
             dirPre += "/";
 
-        string path;
+        std::string path;
 
         path = dirPre + dirNameTmp;
 
@@ -61,11 +60,11 @@ public:
             if (strcmp(pDirEnt->d_name, ".") == 0 || strcmp(pDirEnt->d_name, "..") == 0)
                 continue;
 
-            string tmpDir = dirPre + dirNameTmp + pDirEnt->d_name;
+            std::string tmpDir = dirPre + dirNameTmp + pDirEnt->d_name;
             if (stat(tmpDir.c_str(), &fileStat) != 0)
                 continue;
 
-            string innerDir = dirNameTmp + pDirEnt->d_name;
+            std::string innerDir = dirNameTmp + pDirEnt->d_name;
             if (fileStat.st_mode & S_IFDIR == S_IFDIR)
             {
                 EnumDirFiles(dirPrefix, innerDir, vFiles);
@@ -79,9 +78,9 @@ public:
             closedir(pDir);
     }
 
-    static int writeInZipFile(zipFile zFile, const string& file)
+    static int writeInZipFile(zipFile zFile, const std::string& file)
     {
-        fstream f(file.c_str(), std::ios::binary | std::ios::in);
+        std::fstream f(file.c_str(), std::ios::binary | std::ios::in);
         f.seekg(0, std::ios::end);
         long size = f.tellg();
         f.seekg(0, std::ios::beg);
@@ -110,64 +109,64 @@ public:
             zipFile zFile = zipOpen(dst.c_str(), APPEND_STATUS_CREATE);
             if (zFile == NULL)
             {
-                cout << "openfile failed" << endl;
+                std::cout << "openfile failed" << std::endl;
                 return -1;
             }
             zip_fileinfo zFileInfo = { 0 };
             int ret = zipOpenNewFileInZip(zFile, src.c_str(), &zFileInfo, NULL, 0, NULL, 0, NULL, 0, Z_DEFAULT_COMPRESSION);
             if (ret != ZIP_OK)
             {
-                cout << "openfile in zip failed" << endl;
+                std::cout << "openfile in zip failed" << std::endl;
                 zipClose(zFile, NULL);
                 return -1;
             }
             ret = writeInZipFile(zFile, src);
             if (ret != ZIP_OK)
             {
-                cout << "write in zip failed" << endl;
+                std::cout << "write in zip failed" << std::endl;
                 zipClose(zFile, NULL);
                 return -1;
             }
             zipClose(zFile, NULL);
-            cout << "zip ok" << endl;
+            std::cout << "zip ok" << std::endl;
         }
         else if (S_ISDIR(fileInfo.st_mode))
         {
             size_t pos = src.find_last_of("/");
-            string dirName = src.substr(pos + 1);
-            string dirPrefix = src.substr(0, pos);
+            std::string dirName = src.substr(pos + 1);
+            std::string dirPrefix = src.substr(0, pos);
 
             zipFile zFile = zipOpen(dst.c_str(), APPEND_STATUS_CREATE);
             if (zFile == NULL)
             {
-                cout << "openfile failed" << endl;
+                std::cout << "openfile failed" << std::endl;
                 return -1;
             }
 
-            vector<string> vFiles;
+            std::vector<std::string> vFiles;
             EnumDirFiles(dirPrefix, dirName, vFiles);
-            vector<string>::iterator itF = vFiles.begin();
+            std::vector<std::string>::iterator itF = vFiles.begin();
             for (; itF != vFiles.end(); ++itF)
             {
                 zip_fileinfo zFileInfo = { 0 };
                 int ret = zipOpenNewFileInZip(zFile, itF->c_str(), &zFileInfo, NULL, 0, NULL, 0, NULL, 0, Z_DEFAULT_COMPRESSION);
                 if (ret != ZIP_OK)
                 {
-                    cout << "openfile in zip failed" << endl;
+                    std::cout << "openfile in zip failed" << std::endl;
                     zipClose(zFile, NULL);
                     return -1;
                 }
                 ret = writeInZipFile(zFile, *itF);
                 if (ret != ZIP_OK)
                 {
-                    cout << "write in zip failed" << endl;
+                    std::cout << "write in zip failed" << std::endl;
                     zipClose(zFile, NULL);
                     return -1;
                 }
             }
 
             zipClose(zFile, NULL);
-            cout << "zip ok" << endl;
+            std::cout << "zip ok" << std::endl;
         }
         return 0;
     }
