31889705638fc0a950638b6a3a76707d5927664a
douliyang <734546831@qq.com>
2023-12-27 16:40:41 +0800

    文档格式化

--- su/weather/silly_micaps.cpp
+++ su/weather/silly_micaps.cpp
@@ -7,16 +7,21 @@
 #include <cstring>
 #include <string>
 
-#define SIILY_MICAPS_SKIP_BLANK(s, off, idx)  \
-off = idx;\
-while (s[off] == ' ' || s[off] == '\r' || s[off] == '\n') { off++; }\
-idx = off;\
-while (!(s[idx] == ' ' || s[idx] == '\r' || s[idx] == '\n')) { i++; }\
+#define SILLY_MICAPS_DIAMOND_4_FORMAT "diamond 4"
 
+#define SIILY_MICAPS_SKIP_BLANK(s, off, idx)                     \
+    off = idx;                                                   \
+    while (s[off] == ' ' || s[off] == '\r' || s[off] == '\n')    \
+    {                                                            \
+        off++;                                                   \
+    }                                                            \
+    idx = off;                                                   \
+    while (!(s[idx] == ' ' || s[idx] == '\r' || s[idx] == '\n')) \
+    {                                                            \
+        i++;                                                     \
+    }
 
-#define SILLY_MICAPS_DIAMOND_4_FORMAT   "diamond 4"
-
-bool micaps_diamond_4::check_head(const std::string& content, size_t& offset)
+bool silly_micaps_diamond_4::check_head(const std::string &content, size_t &offset)
 {
     bool status = false;
     offset = strlen(SILLY_MICAPS_DIAMOND_4_FORMAT);
@@ -36,7 +41,7 @@ bool micaps_diamond_4::check_head(const std::string& content, size_t& offset)
     int day = std::stoi(content.substr(offset, i - offset + 1));
     SIILY_MICAPS_SKIP_BLANK(content, offset, i);
     int hour = std::stoi(content.substr(offset, i - offset + 1));
-    char time_buff[32] = { 0 };
+    char time_buff[32] = {0};
     sprintf(time_buff, SU_STD_TIME_FORMAT1, year, month, day, hour, 0, 0);
     base_tm = time_buff;
     SIILY_MICAPS_SKIP_BLANK(content, offset, i);
@@ -74,21 +79,21 @@ bool micaps_diamond_4::check_head(const std::string& content, size_t& offset)
     return status;
 }
 
-bool micaps_diamond_4::is_valid() {
+bool silly_micaps_diamond_4::is_valid()
+{
     bool status = false;
-    // 
+    //
     if (data.size() != lng_size * lat_size)
     {
         return status;
     }
     // TODO: 补充其他 经纬度 经纬步长 等等验证
-    
+
     status = true;
     return status;
 }
 
-
-bool micaps_diamond_4::reset()
+bool silly_micaps_diamond_4::reset()
 {
     // 其他数据清空
     lng_size = 0;
@@ -97,53 +102,54 @@ bool micaps_diamond_4::reset()
     return false;
 }
 
-bool silly_diamond_4::read(const std::string &path, micaps_diamond_4 &md4) {
-	bool status = false;
-	std::string content;
+bool silly_diamond_4::read(const std::string &path, silly_micaps_diamond_4 &md4)
+{
+    bool status = false;
+    std::string content;
     if (!silly_file::read(path, content))
     {
-        return false;
+        return status;
     }
     size_t total_size = content.size();
     size_t offset = 0;
     // 检查文件头
     if (!md4.check_head(content, offset))
     {
-        return false;
+        return status;
     }
 
-    char _dot = '.' -1;
-   
+    char _dot = '.' - 1;
+
     while (offset < total_size)
     {
         if (content[offset] > _dot)
         {
             size_t i = 0;
-            char buff[32] = { 0 };
+            char buff[32] = {0};
             while (content[offset] > _dot)
             {
                 buff[i] = content[offset];
-                i++; offset++;
+                i++;
+                offset++;
             }
             md4.data.push_back(std::stof(buff));
-
         }
         offset++;
     }
     status = md4.is_valid();
     if (!status)
     {
-        md4.reset();  
+        md4.reset();
     }
     return status;
 }
 
-bool silly_diamond_4::mmap_read(const std::string& path, micaps_diamond_4& md4)
+bool silly_diamond_4::mmap_read(const std::string &path, silly_micaps_diamond_4 &md4)
 {
-	return false;
+    return false;
 }
 
-bool silly_diamond_4::write(const std::string& path, const micaps_diamond_4& md4)
+bool silly_diamond_4::write(const std::string &path, const silly_micaps_diamond_4 &md4)
 {
     return false;
 }
--- su/weather/silly_micaps.h
+++ su/weather/silly_micaps.h
@@ -12,16 +12,16 @@
 #include <su_marco.h>
 #include <vector>
 
- /// <summary>
- /// 第四类数据格式：格点数据（文本文件）
- /// 文件头：
- ///     diamond 4  数据说明（字符串）  年  月  日  时次  时效  层次  经度格距  纬度格距  起始经度  终止经度  起始纬度  终止纬度  纬向格点数  经向格点数  等值线间隔  等值线起始值  终止值  平滑系数  加粗线值
- /// 数据：
- ///     数据按先纬向后经向放置，均为浮点数。
- /// 注：
- ///     1）网格必须为经纬度网格
- /// </summary>
-class micaps_diamond_4
+/// <summary>
+/// 第四类数据格式：格点数据（文本文件）
+/// 文件头：
+///     diamond 4  数据说明（字符串）  年  月  日  时次  时效  层次  经度格距  纬度格距  起始经度  终止经度  起始纬度  终止纬度  纬向格点数  经向格点数  等值线间隔  等值线起始值  终止值  平滑系数  加粗线值
+/// 数据：
+///     数据按先纬向后经向放置，均为浮点数。
+/// 注：
+///     1）网格必须为经纬度网格
+/// </summary>
+class silly_micaps_diamond_4
 {
 public:
 	/// <summary>
@@ -30,7 +30,7 @@ public:
 	/// <param name="content"></param>
 	/// <param name="offset"></param>
 	/// <returns></returns>
-	bool check_head(const std::string& content, size_t& offset);
+	bool check_head(const std::string &content, size_t &offset);
 
 	/// <summary>
 	/// 判断已接受数据是否完整并且一致
@@ -48,21 +48,21 @@ public:
 	std::string format;
 	std::string desc;
 	std::string base_tm;
-	size_t aging{ 1 };
-	size_t layer{ 0 };
-	float lng_step{ 0. };
-	float lat_step{ 0. };
-	float left{ 0. };
-	float right{ 0. };
-	float bottom{ 0. };
-	float top{ 0. };
-	size_t lat_size{ 0 };
-	size_t lng_size{ 0 };
-	float iso_step{ 0. };
-	float iso_bv{ 0. };
-	float iso_ev{ 0. };
-	float smooth{ 0. };
-	float bold{ 0. };
+	size_t aging{1};
+	size_t layer{0};
+	float lng_step{0.};
+	float lat_step{0.};
+	float left{0.};
+	float right{0.};
+	float bottom{0.};
+	float top{0.};
+	size_t lat_size{0};
+	size_t lng_size{0};
+	float iso_step{0.};
+	float iso_bv{0.};
+	float iso_ev{0.};
+	float smooth{0.};
+	float bold{0.};
 	std::vector<float> data;
 };
 
@@ -75,7 +75,7 @@ public:
 	/// <param name="path"></param>
 	/// <param name="md4"></param>
 	/// <returns></returns>
-	static bool read(const std::string& path, micaps_diamond_4& md4);
+	static bool read(const std::string &path, silly_micaps_diamond_4 &md4);
 
 	/// <summary>
 	/// 以mmap读取diamond 4格式的数据, 普通读取方式性能已经满足要求,这个暂时不实现
@@ -83,7 +83,7 @@ public:
 	/// <param name="path"></param>
 	/// <param name="md4"></param>
 	/// <returns></returns>
-	static bool mmap_read(const std::string& path, micaps_diamond_4& md4);
+	static bool mmap_read(const std::string &path, silly_micaps_diamond_4 &md4);
 
 	/// <summary>
 	/// 写diamond 4格式的数据
@@ -91,9 +91,9 @@ public:
 	/// <param name="path"></param>
 	/// <param name="md4"></param>
 	/// <returns></returns>
-	static bool write(const std::string& path, const micaps_diamond_4& md4);
-private:
+	static bool write(const std::string &path, const silly_micaps_diamond_4 &md4);
 
+private:
 };
 
 #endif // SILLY_UTILS_SILLY_DIAMOND_4_H
