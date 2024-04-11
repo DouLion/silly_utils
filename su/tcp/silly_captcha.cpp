e471c31f73d0ba9d30764c7620330a911359ae31
douliyang <734546831@qq.com>
2024-04-11 19:05:04 +0800

    调整了文字位置

--- su/tcp/silly_captcha.cpp
+++ su/tcp/silly_captcha.cpp
@@ -12,12 +12,11 @@
 #include FT_FREETYPE_H
 
 // const char SILLY_POSSIBLE_CHAR[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-const char SILLY_POSSIBLE_CHAR[] = "ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789"; // 删除常见容易混淆的字母和数字
-const int SILLY_POSSIBLE_NUM[]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+const char SILLY_POSSIBLE_CHAR[] = "ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";  // 删除常见容易混淆的字母和数字
+const int SILLY_POSSIBLE_NUM[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 const char SILLY_POSSIBLE_OPERATOR[] = "+-×";
 
-
-static cairo_status_t cairo_write_func_mine (void *closure, const unsigned char *data, unsigned int length)
+static cairo_status_t cairo_write_func_mine(void* closure, const unsigned char* data, unsigned int length)
 {
     std::vector<char>& vecData = *((std::vector<char>*)closure);
     for (unsigned int i = 0; i < length; ++i)
@@ -27,11 +26,11 @@ static cairo_status_t cairo_write_func_mine (void *closure, const unsigned char 
     return CAIRO_STATUS_SUCCESS;
 }
 
-static  bool cairo_draw(std::string& result, const std::string text, const std::string font_path, const int font_size=100)
+static bool cairo_draw(std::string& result, const std::string text, const FT_Face* ft_face, const int font_size = 100)
 {
     bool status = false;
     double width = font_size * text.size();
-    if(width == 0 )
+    if (width == 0)
     {
         return status;
     }
@@ -39,44 +38,28 @@ static  bool cairo_draw(std::string& result, const std::string text, const std::
     double height = font_size;
     cairo_surface_t* surface;
     cairo_t* cr;
-    FT_Library ft_library;
-    FT_Face ft_face;
 
-    FT_Error ft_error;
-    bool valid_font = false;
-    if (!font_path.empty())
-    {
-        if (!FT_Init_FreeType(&ft_library))
-        {
-            if (!FT_New_Face(ft_library, font_path.c_str(), 0, &ft_face))
-            {
-                valid_font = true;
-            }
-        }
-    }
-    
-    surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, width, height);
+    surface = cairo_image_surface_create(CAIRO_FORMAT_A8, width, height);
     cr = cairo_create(surface);
-    cairo_set_source_rgb(cr, 1, 1, 1); // RGB 白色
-    cairo_paint(cr); //
+    cairo_set_source_rgba(cr, 0, 0, 0, 0);  // RGB 白色
+    cairo_paint(cr);                    //
     cairo_font_face_t* cairo_font_face = nullptr;
-    if (valid_font) // 
+    if (ft_face)  //
     {
-        cairo_font_face = cairo_ft_font_face_create_for_ft_face(ft_face, 0);
+        cairo_font_face = cairo_ft_font_face_create_for_ft_face(*ft_face, 0);
         cairo_set_font_face(cr, cairo_font_face);
     }
-    
-    cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
-    cairo_set_font_size(cr, font_size); // 设置字体大小
+
+    cairo_set_source_rgba(cr, 0, 0, 0, 1);
+    cairo_set_font_size(cr, font_size);  // 设置字体大小
     cairo_text_extents_t extents;
     cairo_text_extents(cr, text.c_str(), &extents);
-    double font_size_scale = std::min((width / extents.width), (height / extents.height)) * 0.95;
+    double font_size_scale = (height/ extents.height) * 0.8;
     cairo_set_font_size(cr, font_size_scale * font_size);
     double x = (width - extents.width * font_size_scale) / 2.0;
-    double y = height - (height - extents.height * font_size_scale) / 2.0;
-    cairo_move_to(cr, x, y); // 设置文本起始位置
-    cairo_show_text(cr, text.c_str()); // 绘制文本
-    
+    double y = height * 0.85;
+    cairo_move_to(cr, x, y);            // 设置文本起始位置
+    cairo_show_text(cr, text.c_str());  // 绘制文本
 
     std::vector<unsigned char> vecData;
     cairo_surface_write_to_png_stream(surface, cairo_write_func_mine, &vecData);
@@ -85,34 +68,96 @@ static  bool cairo_draw(std::string& result, const std::string text, const std::
 
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
-    if (valid_font)
+    if (ft_face)
     {
         cairo_font_face_destroy(cairo_font_face);
-        FT_Done_Face(ft_face);
-        FT_Done_FreeType(ft_library);
     }
-   
+
     return true;
 }
 
-bool silly_captcha::get_letter_captcha(std::string& answer, std::string& image, const size_t& letter_len, const std::string font_path)
+silly_captcha::silly_captcha()
 {
-    answer = generate_letter_captcha(letter_len);
-    return cairo_draw(image, answer, font_path);
+    if (FT_Init_FreeType(&m_ft_library))
+    {
+    }
 }
-bool silly_captcha::get_math_calc_captcha(std::string& answer, std::string& image, const std::string font_path)
+
+silly_captcha::~silly_captcha()
+{
+    for (auto& [nm, ft] : m_name_font)
+    {
+        FT_Done_Face(ft);
+    }
+    FT_Done_FreeType(m_ft_library);
+}
+
+bool silly_captcha::add_font(const std::string& name, const std::string& path)
+{
+    bool status = false;
+    FT_Face ft_face;
+
+    FT_Error ft_error;
+
+    if (!path.empty())
+    {
+        if (!FT_New_Face(m_ft_library, path.c_str(), 0, &ft_face))
+        {
+            m_name_font[name] = ft_face;
+            status = true;
+        }
+    }
+    return status;
+}
+
+bool silly_captcha::get_letter_captcha(std::string& answer, std::string& image, const size_t& letter_len, const std::string font_name)
+{
+    answer =  generate_letter_captcha(letter_len);
+    if (m_name_font.find(font_name) != std::end(m_name_font))
+    {
+        return cairo_draw(image, answer, &(m_name_font[font_name]));
+    }
+    return cairo_draw(image, answer, nullptr);
+}
+
+bool silly_captcha::get_math_calc_captcha(std::string& answer, std::string& image, const std::string font_name)
 {
     std::string text = generate_math_calc_captcha(answer);
-    return cairo_draw(image, text, font_path);
+    if (m_name_font.find(font_name) != std::end(m_name_font))
+    {
+        return cairo_draw(image, text, &(m_name_font[font_name]));
+    }
+    return cairo_draw(image, text, nullptr);
 }
+
+#define SIILY_CAPTCHAR_TO_LOWER()
+
+bool silly_captcha::is_same(const std::string& src, const std::string& dst)
+{
+    size_t len;
+    if ((len = src.size()) != dst.size())
+    {
+        return false;
+    }
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (std::tolower(static_cast<unsigned char>(src[i])) != std::tolower(static_cast<unsigned char>(dst[i])))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 std::string silly_captcha::generate_letter_captcha(const size_t& letter_len)
 {
     std::string result;
     result.resize(letter_len);
     srand(time(0));
-    for (int i = 0; i < letter_len; i++) {
+    for (int i = 0; i < letter_len; i++)
+    {
         int index = rand() % (sizeof(SILLY_POSSIBLE_CHAR) - 1);
-        result[i]= SILLY_POSSIBLE_CHAR[index];
+        result[i] = SILLY_POSSIBLE_CHAR[index];
     }
     return result;
 }
@@ -120,9 +165,9 @@ std::string silly_captcha::generate_math_calc_captcha(std::string& result)
 {
     std::string text;
     srand(time(0));
-    int ai =  rand() % 10;
-    int bi =  rand() % 10;
-    int oi =  rand() % 3;
+    int ai = rand() % 10;
+    int bi = rand() % 10;
+    int oi = rand() % 3;
 
     int ir = 0;
     switch (oi)
@@ -130,7 +175,7 @@ std::string silly_captcha::generate_math_calc_captcha(std::string& result)
         case 0:
             ir = ai + bi;
             text.append(std::to_string(ai)).append("+").append(std::to_string(bi));
-            break ;
+            break;
         case 1:
             if (ai < bi)
             {
@@ -143,7 +188,7 @@ std::string silly_captcha::generate_math_calc_captcha(std::string& result)
                 text.append(std::to_string(ai)).append("-").append(std::to_string(bi));
             }
             break;
-        case 2:
+        default:
             ir = ai * bi;
             text.append(std::to_string(ai)).append("×").append(std::to_string(bi));
             break;
--- su/tcp/silly_captcha.h
+++ su/tcp/silly_captcha.h
@@ -10,10 +10,22 @@
 #ifndef SILLY_UTILS_SILLY_CAPTCHA_H
 #define SILLY_UTILS_SILLY_CAPTCHA_H
 #include <iostream>
+#include <map>
+#include <cairo/cairo-ft.h>
+#include <singleton/silly_singleton.h>
 
-class silly_captcha
+class silly_captcha : public silly_singleton<silly_captcha>
 {
+    friend class silly_singleton<silly_captcha>;
   public:
+    /// <summary>
+    /// 添加字体
+    /// </summary>
+    /// <param name="name">字体名称</param>
+    /// <param name="path">字体文件路径</param>
+    /// <returns></returns>
+    bool add_font(const std::string& name, const std::string& path);
+
     /// <summary>
     /// 获取指定长度的数字+字母字符串的验证码
     /// </summary>
@@ -22,7 +34,7 @@ class silly_captcha
     /// <param name="letter_len">指定字母长度</param>
     /// <param name="font_path">指定字体文件位置</param>
     /// <returns></returns>
-    static bool get_letter_captcha(std::string& answer, std::string& image, const size_t& letter_len = 4, const std::string font_path="./RubikMicrobe.ttf");
+    bool get_letter_captcha(std::string& answer, std::string& image, const size_t& letter_len = 4, const std::string font_name="");
 
     /// <summary>
     /// 获取一个简单10以内加减乘计算的验证码
@@ -31,11 +43,25 @@ class silly_captcha
     /// <param name="image">图像</param>
     /// <param name="font_path">指定字体文件位置</param>
     /// <returns></returns>
-    static bool get_math_calc_captcha(std::string& answer, std::string& image, const std::string font_path="./RubikMicrobe.ttf");
+    bool get_math_calc_captcha(std::string& answer, std::string& image, const std::string font_name = "");
+
+    /// <summary>
+    /// 比较是否相等,不区分大小写
+    /// </summary>
+    /// <param name="src"></param>
+    /// <param name="dst"></param>
+    /// <returns></returns>
+    bool is_same(const std::string& src, const std::string& dst);
 
   private:
-    static std::string generate_letter_captcha(const size_t& letter_len = 4);
-    static std::string generate_math_calc_captcha(std::string& result);
+    silly_captcha();
+    ~silly_captcha();
+    std::string generate_letter_captcha(const size_t& letter_len = 4);
+    std::string generate_math_calc_captcha(std::string& result);
+
+  private:
+    std::map<std::string, FT_Face> m_name_font;
+    FT_Library m_ft_library;
 
 };
 
