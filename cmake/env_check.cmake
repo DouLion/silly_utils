acb61d6b037f2480fe53e4f946500ecd509a0f5a
DouLion <734546831@qq.com>
2024-09-13 14:13:00 +0800

    格式化cmake文件

--- cmake/env_check.cmake
+++ cmake/env_check.cmake
@@ -1,10 +1,10 @@
 cmake_minimum_required(VERSION 3.15)
 
 if(CMP0074)
-    cmake_policy(SET CMP0074 NEW)
+  cmake_policy(SET CMP0074 NEW)
 endif()
 if(CMP0167)
-    cmake_policy(SET CMP0167 NEW)
+  cmake_policy(SET CMP0167 NEW)
 endif()
 
 include(CheckIncludeFileCXX)
@@ -14,76 +14,75 @@ set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
 if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-    set(CMAKE_BUILD_TYPE "Debug")
-    message("AUTO SET CMAKE_BUILD_TYPE : Debug")
+  set(CMAKE_BUILD_TYPE "Debug")
+  message("AUTO SET CMAKE_BUILD_TYPE : Debug")
 endif()
 
 # 系统信息
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
-    message(STATUS "This is Linux")
-    set(IS_LINUX TRUE)
-    set(PLATFORM_NAME "Linux")
+  message(STATUS "This is Linux")
+  set(IS_LINUX TRUE)
+  set(PLATFORM_NAME "Linux")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Android")
-	message(STATUS "This is Android")
-elseIF (CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
-    message(STATUS "This is Windows")
-    set(PLATFORM_NAME "Windows")
-    set(IS_WINDOWS TRUE)
+  message(STATUS "This is Android")
+elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
+  message(STATUS "This is Windows")
+  set(PLATFORM_NAME "Windows")
+  set(IS_WINDOWS TRUE)
 endif()
 
 if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "aarch64")
-    message(STATUS "this is aarch64 cpu")
+  message(STATUS "this is aarch64 cpu")
 elseif(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "x86_64")
-	message(STATUS "this is x86_64 cpu")
+  message(STATUS "this is x86_64 cpu")
 endif()
 
 # 检查系统位数
-IF (CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(ARCH_NAME "x64")
-    message(STATUS "Target is 64 bits")
-else ()
-    set(ARCH_NAME "x32")
-    message(STATUS "Target is 32 bits")
-    message(FATAL_ERROR "Not support on 32 bit platform")
-endif ()
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(ARCH_NAME "x64")
+  message(STATUS "Target is 64 bits")
+else()
+  set(ARCH_NAME "x32")
+  message(STATUS "Target is 32 bits")
+  message(FATAL_ERROR "Not support on 32 bit platform")
+endif()
 
 # message("当前C++编译器支持的C++ 特性:")
 foreach(feature ${CMAKE_CXX_COMPILE_FEATURES})
-    if("${feature}" STREQUAL "cxx_std_11" )
-        add_definitions("-DSU_SUPPORT_CXX11=1")
-        set(SU_SUPPORT_CXX11 TRUE)
-    endif()
-    if("${feature}" STREQUAL "cxx_std_17" )
-        add_definitions("-DSU_SUPPORT_CXX17=1")
-        set(SU_SUPPORT_CXX17 TRUE)
-    endif()
-    if("${feature}" STREQUAL "cxx_std_20" )
-        # add_definitions("-DSU_SUPPORT_CXX20=1")
-        # set(SU_SUPPORT_CXX20 TRUE)
-    endif()
-    
+  if("${feature}" STREQUAL "cxx_std_11")
+    add_definitions("-DSU_SUPPORT_CXX11=1")
+    set(SU_SUPPORT_CXX11 TRUE)
+  endif()
+  if("${feature}" STREQUAL "cxx_std_17")
+    add_definitions("-DSU_SUPPORT_CXX17=1")
+    set(SU_SUPPORT_CXX17 TRUE)
+  endif()
+  if("${feature}" STREQUAL "cxx_std_20")
+    # add_definitions("-DSU_SUPPORT_CXX20=1") set(SU_SUPPORT_CXX20 TRUE)
+  endif()
+
 endforeach()
 
 # c++ 支持版本
 if(SU_SUPPORT_CXX20)
-    set(CMAKE_CXX_STANDARD 20)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set(CMAKE_CXX_EXTENSIONS OFF)
-    # c++ 17 已经包含一些类型定义了,可能会导致重复定义
-    add_definitions("-D_HAS_STD_BYTE=0")
-    message("使用C++20标准")
+  set(CMAKE_CXX_STANDARD 20)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  set(CMAKE_CXX_EXTENSIONS OFF)
+  # c++ 17 已经包含一些类型定义了,可能会导致重复定义
+  add_definitions("-D_HAS_STD_BYTE=0")
+  message("使用C++20标准")
 elseif(SU_SUPPORT_CXX17)
-    set(CMAKE_CXX_STANDARD 17)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set(CMAKE_CXX_EXTENSIONS OFF)
-    # c++ 17 已经包含一些类型定义了,可能会导致重复定义
-    add_definitions("-D_HAS_STD_BYTE=0")
-    message("使用C++17标准")
+  set(CMAKE_CXX_STANDARD 17)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  set(CMAKE_CXX_EXTENSIONS OFF)
+  # c++ 17 已经包含一些类型定义了,可能会导致重复定义
+  add_definitions("-D_HAS_STD_BYTE=0")
+  message("使用C++17标准")
 elseif(SU_SUPPORT_CXX11)
-    set(CMAKE_CXX_STANDARD 11)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set(CMAKE_CXX_EXTENSIONS OFF)
-    message("使用C++11标准")
+  set(CMAKE_CXX_STANDARD 11)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  set(CMAKE_CXX_EXTENSIONS OFF)
+  message("使用C++11标准")
 endif()
 
 add_definitions("-DENABLE_SILLY_LOG")
@@ -93,54 +92,60 @@ add_definitions("-DNOMINMAX")
 add_definitions("-DUNICODE")
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
 
-    set(CMAKE_THREAD_LIBS_INIT "-lpthread")
-    set(CMAKE_HAVE_THREADS_LIBRARY 1)
-    set(CMAKE_USE_WIN32_THREADS_INIT 0)
-    set(CMAKE_USE_PTHREADS_INIT 1)
-    set(THREADS_PREFER_PTHREAD_FLAG ON)
+  set(CMAKE_THREAD_LIBS_INIT "-lpthread")
+  set(CMAKE_HAVE_THREADS_LIBRARY 1)
+  set(CMAKE_USE_WIN32_THREADS_INIT 0)
+  set(CMAKE_USE_PTHREADS_INIT 1)
+  set(THREADS_PREFER_PTHREAD_FLAG ON)
 
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -lpthread -fPIC -Wall -fopenmp -Wno-unused-function -Wno-unused-variable -lodbc")
-    set(STATIC_LIB_SUFFIX "a")
-    set(DYNAMIC_LIB_SUFFIX "so")
+  set(CMAKE_CXX_FLAGS
+      "${CMAKE_CXX_FLAGS} -std=c++17 -lpthread -fPIC -Wall -fopenmp -Wno-unused-function -Wno-unused-variable -lodbc"
+  )
+  set(STATIC_LIB_SUFFIX "a")
+  set(DYNAMIC_LIB_SUFFIX "so")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
 
-    add_definitions("-DIS_WIN32")
-    add_definitions("-DWIN32_LEAN_AND_MEAN")
-    add_compile_options(/wd4819 /wd4005 /wd4834 /wd4996 /utf-8 /openmp)
-    set(STATIC_LIB_SUFFIX "lib")
-    set(DYNAMIC_LIB_SUFFIX "dll")
+  add_definitions("-DIS_WIN32")
+  add_definitions("-DWIN32_LEAN_AND_MEAN")
+  add_compile_options(/wd4819 /wd4005 /wd4834 /wd4996 /utf-8 /openmp)
+  set(STATIC_LIB_SUFFIX "lib")
+  set(DYNAMIC_LIB_SUFFIX "dll")
 
-    if(MSVC)
-        # SET cmake cxx flags.
-        set(CMAKE_CXX_FLAGS_DEBUG "")
-        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /D_DEBUG /MDd /Zi /Ob0  /Od /RTC1 /Gy /EHsc")
+  if(MSVC)
+    # SET cmake cxx flags.
+    set(CMAKE_CXX_FLAGS_DEBUG "")
+    set(CMAKE_CXX_FLAGS_DEBUG
+        "${CMAKE_CXX_FLAGS_DEBUG} /D_DEBUG /MDd /Zi /Ob0  /Od /RTC1 /Gy /EHsc")
 
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "")
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} /MD /Zi /O1 /Ob2 /Oi /Os /D NDEBUG /GS- ")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL "")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL
+        "${CMAKE_CXX_FLAGS_MINSIZEREL} /MD /Zi /O1 /Ob2 /Oi /Os /D NDEBUG /GS- "
+    )
 
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "")
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /MD /Zi /O2 /Ob1 /D NDEBUG ")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO
+        "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /MD /Zi /O2 /Ob1 /D NDEBUG ")
 
-        set(CMAKE_CXX_FLAGS_RELEASE "")
-        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MD /Zi /O2 /Ob1 /D NDEBUG ")
-    endif(MSVC)
+    set(CMAKE_CXX_FLAGS_RELEASE "")
+    set(CMAKE_CXX_FLAGS_RELEASE
+        "${CMAKE_CXX_FLAGS_RELEASE} /MD /Zi /O2 /Ob1 /D NDEBUG ")
+  endif(MSVC)
 
 endif()
 
 find_package(Boost COMPONENTS program_options date_time)
 if(Boost_FOUND)
-    # 如果找到了 Boost 库
-    include_directories(${Boost_INCLUDE_DIRS})
-    add_definitions("-DSILLY_UTILS_BOOST_ENABLE")
-    message("Found boost  ${Boost_INCLUDE_DIRS}")
+  # 如果找到了 Boost 库
+  include_directories(${Boost_INCLUDE_DIRS})
+  add_definitions("-DSILLY_UTILS_BOOST_ENABLE")
+  message("Found boost  ${Boost_INCLUDE_DIRS}")
 else()
-    message(WARNING "Boost library not found. Some features might be disabled.")
+  message(WARNING "Boost library not found. Some features might be disabled.")
 endif()
 
-
-
-##########################################
+# ##############################################################################
 # 设置全局的可执行程序和链接库的生成路径.
-##########################################
-set(EXECUTABLE_OUTPUT_PATH "${CMAKE_SOURCE_DIR}/Bin/${PLATFORM_NAME}/${ARCH_NAME}/${CMAKE_BUILD_TYPE}")
+# ##############################################################################
+set(EXECUTABLE_OUTPUT_PATH
+    "${CMAKE_SOURCE_DIR}/Bin/${PLATFORM_NAME}/${ARCH_NAME}/${CMAKE_BUILD_TYPE}")
 set(LIBRARY_OUTPUT_PATH "${EXECUTABLE_OUTPUT_PATH}")
--- cmake/silly_utils.cmake
+++ cmake/silly_utils.cmake
@@ -2,41 +2,58 @@
 include(FetchContent)
 # FetchContent_MakeAvailable was not added until CMake 3.14
 if(${CMAKE_VERSION} VERSION_LESS 3.14)
-    include(add_FetchContent_MakeAvailable.cmake)
+  include(add_FetchContent_MakeAvailable.cmake)
 endif()
 
 # 设置silly_utils的下载目录
 if("${SILLY_UTILS_FETCH_ROOT}" STREQUAL "")
-    set(SILLY_UTILS_FETCH_ROOT ${CMAKE_SOURCE_DIR}/su_ext)
+  set(SILLY_UTILS_FETCH_ROOT ${CMAKE_SOURCE_DIR}/su_ext)
 endif()
 
-# 指定git仓库地址, 如果能连接192.168.0.60,使用该地址,速度会快一点,如果不能,使用123.56.193.136:16080,这个是.net的公网地址
+# 指定git仓库地址,
+# 如果能连接192.168.0.60,使用该地址,速度会快一点,
+# 如果不能,使用123.56.193.136:16080,这个是.net的公网地址
 if("${SILLY_UTILS_FETCH_IP_PORT}" STREQUAL "")
-    set(SILLY_UTILS_FETCH_IP_PORT "192.168.0.60")
+  set(SILLY_UTILS_FETCH_IP_PORT "192.168.0.60")
 endif()
 
 # 指定版本
-set(SILLY_UTIL_TAG  master)  
-set(SILLY_UTILS_GIT_URL  "http://${SILLY_UTILS_FETCH_IP_PORT}/douliyang/silly_utils.git")  
-
+set(SILLY_UTIL_TAG master)
+set(SILLY_UTILS_GIT_URL
+    "http://${SILLY_UTILS_FETCH_IP_PORT}/douliyang/silly_utils.git")
 
 FetchContent_Declare(
   silly_utils
-  GIT_REPOSITORY    ${SILLY_UTILS_GIT_URL}
-  SOURCE_DIR        ${SILLY_UTILS_FETCH_ROOT}/silly_utils
-)
+  GIT_REPOSITORY ${SILLY_UTILS_GIT_URL}
+  SOURCE_DIR ${SILLY_UTILS_FETCH_ROOT}/silly_utils)
 
 FetchContent_MakeAvailable(silly_utils)
 
 # 下载完成后 拷贝检测环境的.cmake文件
-IF(NOT EXISTS "${CMAKE_SOURCE_DIR}/su_cmake")
-    FILE(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/su_cmake")
-ENDIF()
-execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/env_check.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/env_check.cmake")
-execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/.clang-format" "${CMAKE_SOURCE_DIR}/.clang-format")
+if(NOT EXISTS "${CMAKE_SOURCE_DIR}/su_cmake")
+  file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/su_cmake")
+endif()
+execute_process(
+  COMMAND
+    ${CMAKE_COMMAND} -E copy
+    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/env_check.cmake"
+    "${CMAKE_SOURCE_DIR}/su_cmake/env_check.cmake")
+execute_process(
+  COMMAND
+    ${CMAKE_COMMAND} -E copy
+    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/.clang-format"
+    "${CMAKE_SOURCE_DIR}/.clang-format")
 # 下面这两行需要确认一下是否可以删除
-execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/dm8_check.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/dm8_check.cmake")
-execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/grib_api_check.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/grib_api_check.cmake")
+execute_process(
+  COMMAND
+    ${CMAKE_COMMAND} -E copy
+    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/dm8_check.cmake"
+    "${CMAKE_SOURCE_DIR}/su_cmake/dm8_check.cmake")
+execute_process(
+  COMMAND
+    ${CMAKE_COMMAND} -E copy
+    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/grib_api_check.cmake"
+    "${CMAKE_SOURCE_DIR}/su_cmake/grib_api_check.cmake")
 
 set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/su_cmake;${CMAKE_MODULE_PATH}")
 include(env_check)
