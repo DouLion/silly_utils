8fb1785a2def1ef856c4e58e83a7a9047c5e1aa2
DouLion <734546831@qq.com>
2024-09-26 13:56:50 +0800

    其他项目引用方式

--- cmake/silly_utils.cmake
+++ cmake/silly_utils.cmake
@@ -2,63 +2,51 @@
 include(FetchContent)
 # FetchContent_MakeAvailable was not added until CMake 3.14
 if(${CMAKE_VERSION} VERSION_LESS 3.14)
-  include(add_FetchContent_MakeAvailable.cmake)
+    include(add_FetchContent_MakeAvailable.cmake)
 endif()
 
 # 设置silly_utils的下载目录
 if("${SILLY_UTILS_FETCH_ROOT}" STREQUAL "")
-  set(SILLY_UTILS_FETCH_ROOT ${CMAKE_SOURCE_DIR}/su_ext)
+    set(SILLY_UTILS_FETCH_ROOT ${CMAKE_SOURCE_DIR}/su_ext)
 endif()
 
-# 指定git仓库地址,
-# 如果能连接192.168.0.60,使用该地址,速度会快一点,
-# 如果不能,使用123.56.193.136:16080,这个是.net的公网地址
+# 指定git仓库地址, 如果能连接192.168.0.60,使用该地址,速度会快一点,如果不能,使用123.56.193.136:16080,这个是.net的公网地址
 if("${SILLY_UTILS_FETCH_IP_PORT}" STREQUAL "")
-  set(SILLY_UTILS_FETCH_IP_PORT "192.168.0.60")
+    set(SILLY_UTILS_FETCH_IP_PORT "192.168.0.60")
 endif()
 
 # 指定版本
-set(SILLY_UTIL_TAG master)
-set(SILLY_UTILS_GIT_URL
-    "http://${SILLY_UTILS_FETCH_IP_PORT}/douliyang/silly_utils.git")
+set(SILLY_UTIL_TAG  master)
+set(SILLY_UTILS_GIT_URL  "http://${SILLY_UTILS_FETCH_IP_PORT}/douliyang/silly_utils.git")
+
 
 FetchContent_Declare(
-  silly_utils
-  GIT_REPOSITORY ${SILLY_UTILS_GIT_URL}
-  SOURCE_DIR ${SILLY_UTILS_FETCH_ROOT}/silly_utils)
+        silly_utils
+        GIT_REPOSITORY    ${SILLY_UTILS_GIT_URL}
+        SOURCE_DIR        ${SILLY_UTILS_FETCH_ROOT}/silly_utils
+)
 
 FetchContent_MakeAvailable(silly_utils)
 
 # 下载完成后 拷贝检测环境的.cmake文件
 if(NOT EXISTS "${CMAKE_SOURCE_DIR}/su_cmake")
-  file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/su_cmake")
+    FILE(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/su_cmake")
+else()
+    FILE(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/su_cmake")
 endif()
-execute_process(
-  COMMAND
-    ${CMAKE_COMMAND} -E copy
-    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckEnv.cmake"
-    "${CMAKE_SOURCE_DIR}/su_cmake/CheckEnv.cmake")
-execute_process(
-  COMMAND
-    ${CMAKE_COMMAND} -E copy
-    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/.clang-format"
-    "${CMAKE_SOURCE_DIR}/.clang-format")
-# 下面这两行需要确认一下是否可以删除
-execute_process(
-  COMMAND
-    ${CMAKE_COMMAND} -E copy
-    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/dm8_check.cmake"
-    "${CMAKE_SOURCE_DIR}/su_cmake/dm8_check.cmake")
-execute_process(
-  COMMAND
-    ${CMAKE_COMMAND} -E copy
-    "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/eccodes_check.cmake"
-    "${CMAKE_SOURCE_DIR}/su_cmake/eccodes_check.cmake")
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/.clang-format" "${CMAKE_SOURCE_DIR}/.clang-format")
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckEnv.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/CheckEnv.cmake")
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckOSEndian.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/CheckOSEndian.cmake")
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckDM8.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/CheckDM8.cmake")
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckEccodes.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/CheckEccodes.cmake")
+execute_process(COMMAND ${CMAKE_COMMAND} -E  copy "${SILLY_UTILS_FETCH_ROOT}/silly_utils/cmake/CheckCUDA.cmake" "${CMAKE_SOURCE_DIR}/su_cmake/CheckCUDA.cmake")
 
 set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/su_cmake;${CMAKE_MODULE_PATH}")
 include(CheckEnv)
+include(CheckOSEndian)
 include(CheckDM8)
-include(CheckCUDA)
 include(CheckEccodes)
+include(CheckCUDA)
 
 include_directories("${SILLY_UTILS_FETCH_ROOT}/silly_utils/su")
