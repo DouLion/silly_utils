b5c628d76e5c06f809ed7f860a4552764ad36ed3
DouLion <734546831@qq.com>
2024-10-09 16:30:40 +0800

    准备添加LoRa通信

---
+++ su/network/lora/silly_lora_client.h
@@ -0,0 +1,23 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2024-10-09
+ * @file: silly_lora_client.h
+ * @description: silly_lora_client 类声明
+ * @version: v1.0.1 2024-10-09 dou li yang
+ */
+#ifndef SILLY_UTILS_SILLY_LORA_CLIENT_H
+#define SILLY_UTILS_SILLY_LORA_CLIENT_H
+
+class silly_lora_client
+{
+  public:
+    silly_lora_client();
+    ~silly_lora_client();
+
+  private:
+};
+
+#endif  // SILLY_UTILS_SILLY_LORA_CLIENT_H
---
+++ su/network/lora/silly_lora_gateway.h
@@ -0,0 +1,23 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2024-10-09
+ * @file: silly_lora_gateway.h
+ * @description: silly_lora_gateway 类声明
+ * @version: v1.0.1 2024-10-09 dou li yang
+ */
+#ifndef SILLY_UTILS_SILLY_LORA_GATEWAY_H
+#define SILLY_UTILS_SILLY_LORA_GATEWAY_H
+
+class silly_lora_gateway
+{
+  public:
+    silly_lora_gateway();
+    ~silly_lora_gateway();
+
+  private:
+};
+
+#endif  // SILLY_UTILS_SILLY_LORA_GATEWAY_H
