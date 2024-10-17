d3d9ad45570d17c928df90ab0377ce80a7e2ad40
DouLion <734546831@qq.com>
2024-10-17 16:20:38 +0800

    fix g++

--- su/tzx/moisture/silly_moisture.h
+++ su/tzx/moisture/silly_moisture.h
@@ -32,9 +32,8 @@ class soil_moisture_record
 
     std::string serialize() const;
     bool deserialize(const std::string& data);
-    const static size_t serialized_size;
+    static constexpr size_t serialized_size = 4 + 4 + 4 + 4 + 1;
 };
-const static size_t soil_moisture_record::serialized_size = 4 + 4 + 4 + 4 + 1;
 class silly_moisture_index
 {
   public:
