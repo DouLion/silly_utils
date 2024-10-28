bed568b4866befc479190191979fbb20e4277574
DouLion <734546831@qq.com>
2024-10-28 16:08:20 +0800

    添加金子塔相关的坐标转化

--- su/geo/silly_pyramid.cpp
+++ su/geo/silly_pyramid.cpp
@@ -1,29 +1,114 @@
 #include "silly_pyramid.h"
 
-google_pyramid_block google_pyramid_util::get_block_rect(size_t l, size_t c, size_t r)
+silly_rect silly_google_pyramid::get_rect(const pyramid_index& index)
 {
-    google_pyramid_block ret_block(l, c, r);
-    int ceil_num = pow(2, l);  // 该层的行数列数
+    silly_rect ret;
+    size_t ceil_num = static_cast<size_t>(2 << index.layer);  // 该层的行数列数
+    if (!(index.row < ceil_num && index.col < ceil_num))
+    {
+        throw std::runtime_error("行列号不能超过 " + std::to_string(ceil_num));
+    }
     double x_delta = (SILLY_GLOBAL_RIGHT - SILLY_GLOBAL_LEFT) / ceil_num;
     double y_delta = (SILLY_GLOBAL_TOP - SILLY_GLOBAL_BOTTOM) / ceil_num;
-    ret_block.rect.min.x = c * x_delta + (0 - SILLY_GLOBAL_LEFT);  // 原点x方向上的偏移量
-    ret_block.rect.max.x = ret_block.rect.min.x + x_delta;
-    ret_block.rect.max.y = SILLY_GLOBAL_TOP - y_delta * r;  // 方向上由上到下, 与经纬度表示的相反, 并且减去y方向上的偏移量
-    ret_block.rect.min.y = ret_block.rect.max.y - y_delta;
+    ret.min.x = index.col * x_delta + SILLY_GLOBAL_LEFT;  // 原点x方向上的偏移量
+    ret.max.x = ret.min.x + x_delta;
+    ret.max.y = SILLY_GLOBAL_TOP - y_delta * index.row;  // 方向上由上到下, 与经纬度表示的相反, 并且减去y方向上的偏移量
+    ret.min.y = ret.max.y - y_delta;
 
-    return ret_block;
+    return ret;
+}
+pyramid_index silly_google_pyramid::get_index(const size_t& layer, const silly_point& point)
+{
+    if (point.x < SILLY_GLOBAL_LEFT || point.x > SILLY_GLOBAL_RIGHT || point.y < SILLY_GLOBAL_BOTTOM || point.y > SILLY_GLOBAL_TOP)
+    {
+        throw std::runtime_error("经纬度超出范围 x:" + std::to_string(point.x) + " y:" + std::to_string(point.y));
+    }
+    pyramid_index ret;
+    ret.layer = layer;
+    int ceil_num = (2 << layer);  // 该层的行数列数
+    double x_delta = (SILLY_GLOBAL_RIGHT - SILLY_GLOBAL_LEFT) / ceil_num;
+    double y_delta = (SILLY_GLOBAL_TOP - SILLY_GLOBAL_BOTTOM) / ceil_num;
+    ret.col = ceil((point.x - SILLY_GLOBAL_LEFT) / x_delta);
+    ret.row = ceil((SILLY_GLOBAL_TOP - point.y) / y_delta);
+    return ret;
 }
 
-google_pyramid_block google_pyramid_util::get_point_block(const size_t layer, const silly_point& point)
+pyramid_index silly_pyramid_proj::geo2pyramid(const uint8_t& layer, const silly_point& gp, const uint64_t& tilesize)
 {
-    // TODO : 完成函数, 这两个可能不太好用, 再考虑
-    google_pyramid_block ret_block;
+    pyramid_index ret;
 
-    int ceil_num = pow(2, layer);  // 该层的行数列数
+    screen_point sp = geo2screen(layer, gp, tilesize);
+    ret = screen2pyramid(layer, sp, tilesize);
+    return ret;
+}
+
+silly_rect silly_pyramid_proj::pyramid2geo(const pyramid_index& index, const uint64_t& tilesize)
+{
+    silly_rect ret;
+    size_t ceil_num = static_cast<size_t>(2 << index.layer);  // 该层的行数列数
     double x_delta = (SILLY_GLOBAL_RIGHT - SILLY_GLOBAL_LEFT) / ceil_num;
     double y_delta = (SILLY_GLOBAL_TOP - SILLY_GLOBAL_BOTTOM) / ceil_num;
+    ret.min.x = index.col * x_delta + SILLY_GLOBAL_LEFT;  // 原点x方向上的偏移量
+    ret.max.x = ret.min.x + x_delta;
+    ret.max.y = SILLY_GLOBAL_TOP - y_delta * index.row;  // 方向上由上到下, 与经纬度表示的相反, 并且减去y方向上的偏移量
+    ret.min.y = ret.max.y - y_delta;
 
-    // int col =
+    return ret;
+}
 
-    return ret_block;
+screen_point silly_pyramid_proj::geo2screen(const uint8_t& layer, const silly_point& gp, const uint64_t& tilesize)
+{
+    screen_point ret;
+    double total_tilesize = tilesize << layer;  // 第layer层的瓦片大小
+    ret.layer = layer;
+    ret.sr = std::floor(total_tilesize * (1.0 - (log(tan((gp.y * PI / 180.0) / 2.0 + PI / 4.0)) / PI)) / 2.0);
+    ret.sc = std::floor((gp.x + 180.0) / 360.0 * total_tilesize);
+    return ret;
+}
+
+screen_rect silly_pyramid_proj::geo2screen(const uint8_t& layer, const silly_rect& rect, const uint64_t& tilesize)
+{
+    screen_rect ret;
+    double total_tilesize = tilesize << layer;  // 第layer层的瓦片大小
+    ret.layer = layer;
+    ret.min.sr = std::floor(total_tilesize * (1.0 - (log(tan((rect.min.y * PI / 180.0) / 2.0 + PI / 4.0)) / PI)) / 2.0);
+    ret.min.sc = std::floor((rect.min.x + 180.0) / 360.0 * total_tilesize);
+    ret.max.sr = std::ceil(total_tilesize * (1.0 - (log(tan((rect.max.y * PI / 180.0) / 2.0 + PI / 4.0)) / PI)) / 2.0);
+    ret.max.sc = std::ceil((rect.max.x + 180.0) / 360.0 * total_tilesize);
+
+    return ret;
+}
+
+silly_point silly_pyramid_proj::screen2geo(const uint8_t& layer, const screen_point& sp, const uint64_t& tilesize)
+{
+    silly_point ret;
+    double total_tilesize = tilesize << layer;
+    // 计算经度
+    ret.x = (sp.sc / total_tilesize) * 360.0 - 180.0;
+
+    // 计算纬度
+    double n = PI - (2.0 * PI * sp.sr) / total_tilesize;
+    double latRad = atan(sinh(n));
+    ret.y = latRad * 180.0 / PI;
+    return ret;
+}
+
+pyramid_index silly_pyramid_proj::screen2pyramid(const uint8_t& layer, const screen_point& sp, const uint64_t& tilesize)
+{
+    pyramid_index ret;
+    ret.row = sp.sr % tilesize;
+    ret.col = sp.sc % tilesize;
+
+    return ret;
+}
+
+screen_rect silly_pyramid_proj::pyramid2screen(const pyramid_index& index, const uint64_t& tilesize)
+{
+    screen_rect ret;
+    ret.layer = index.layer;
+    ret.min.sr = index.row * tilesize;
+    ret.min.sc = index.col * tilesize;
+    ret.max.sr = (index.row + 1) * tilesize;
+    ret.max.sc = (index.col + 1) * tilesize;
+    return ret;
 }
--- su/geo/silly_pyramid.h
+++ su/geo/silly_pyramid.h
@@ -16,35 +16,40 @@
 
 #define SILLY_GLOBAL_LEFT -180.0
 #define SILLY_GLOBAL_RIGHT 180.0
-#define SILLY_GLOBAL_LNG_LEN 360.0
 #define SILLY_GLOBAL_TOP 90.0
 #define SILLY_GLOBAL_BOTTOM -90.0
 
-/// <summary>
-/// 世界地图左上角为原点, 右下角为 (max, max)
-/// </summary>
-class google_pyramid_block
+/**
+           90
+
+ -180                 180
+
+           -90
+
+**/
+
+struct screen_point
 {
-  public:
-    google_pyramid_block() = default;
-    google_pyramid_block(size_t& l, size_t& r, size_t& c)
-    {
-        layer = l;
-        col = c;
-        row = r;
-    }
-    // 层号,行列号 从0 开始
-    size_t layer{0};
-    size_t col{0};
-    size_t row{0};
-    // TODO: 该层的图像的宽高, 有个计算公式
-    size_t width;
-    size_t height;
-    // 经纬范围
-    silly_geo_rect rect;
+    uint8_t layer;
+    uint64_t sr;  // 屏幕的行
+    uint64_t sc;  // 屏幕的列
 };
 
-class google_pyramid_util
+struct screen_rect
+{
+    uint8_t layer;
+    screen_point max;
+    screen_point min;
+};
+
+struct pyramid_index
+{
+    uint8_t layer;
+    uint64_t col;
+    uint64_t row;
+};
+
+class silly_google_pyramid
 {
   public:
     /// <summary>
@@ -54,7 +59,7 @@ class google_pyramid_util
     /// <param name="c"></param>
     /// <param name="r"></param>
     /// <returns></returns>
-    static google_pyramid_block get_block_rect(size_t l, size_t c, size_t r);
+    static silly_rect get_rect(const pyramid_index& index);
 
     /// <summary>
     /// 坐标点在指定层所在的块信息
@@ -62,7 +67,73 @@ class google_pyramid_util
     /// <param name="layer"></param>
     /// <param name="point"></param>
     /// <returns></returns>
-    static google_pyramid_block get_point_block(const size_t layer, const silly_point& point);
+    static pyramid_index get_index(const size_t& layer, const silly_point& point);
+};
+
+class silly_pyramid_proj
+{
+  public:
+    /// <summary>
+    /// 经纬度所在的块
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <returns></returns>
+    static pyramid_index geo2pyramid(const uint8_t& layer, const silly_point& geo, const uint64_t& tilesize = 256);
+
+    /// <summary>
+    /// 瓦片所在的经纬度范围
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <returns></returns>
+    static silly_rect pyramid2geo(const pyramid_index& index, const uint64_t& tilesize = 256);
+
+    /// <summary>
+    /// 经纬度对应的屏幕位置
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <param name="nth2">瓦片宽高均为 2^nth2</param>
+    /// <returns></returns>
+    static screen_point geo2screen(const uint8_t& layer, const silly_point& geo, const uint64_t& tilesize = 256);
+
+    /// <summary>
+    /// 经纬度对应的屏幕位置
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <param name="nth2">瓦片宽高均为 2^nth2</param>
+    /// <returns></returns>
+    static screen_rect geo2screen(const uint8_t& layer, const silly_rect& rect, const uint64_t& tilesize = 256);
+
+    /// <summary>
+    /// 屏幕位置对应的经纬度
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <param name="nth2">瓦片宽高均为 2^nth2</param>
+    /// <returns></returns>
+    static silly_point screen2geo(const uint8_t& layer, const screen_point& point, const uint64_t& tilesize = 256);
+
+    /// <summary>
+    /// 屏幕位置对应的瓦片
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <param name="nth2">瓦片宽高均为 2^nth2</param>
+    /// <returns></returns>
+    static pyramid_index screen2pyramid(const uint8_t& layer, const screen_point& point, const uint64_t& tilesize = 256);
+
+
+    /// <summary>
+    /// 瓦片索引对应的屏幕范围
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <param name="geo"></param>
+    /// <param name="nth2">瓦片宽高均为 2^nth2</param>
+    /// <returns></returns>
+    static screen_rect pyramid2screen(const pyramid_index& index, const uint64_t& tilesize = 256);
 };
 
 #endif  // SILLY_UTILS_SILLY_PYRAMID_H
