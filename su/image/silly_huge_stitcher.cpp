e41999cf6d4aff747bddeb89461d4867166dc13d
DouLion <734546831@qq.com>
2024-11-05 12:11:32 +0800

    巨型图像拼接,依赖gdal实现

---
+++ su/image/silly_huge_stitcher.cpp
@@ -0,0 +1,11 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2024-11-05
+ * @file: silly_huge_stitcher.cpp
+ * @description: silly_huge_stitcher实现
+ * @version: v1.0.1 2024-11-05 dou li yang
+ */
+#include "silly_huge_stitcher.h"
---
+++ su/image/silly_huge_stitcher.h
@@ -0,0 +1,23 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2024-11-05
+ * @file: silly_huge_stitcher.h
+ * @description:  超大图像拼接
+ * @version: v1.0.1 2024-11-05 dou li yang
+ */
+#ifndef SILLY_UTILS_SILLY_HUGE_STITCHER_H
+#define SILLY_UTILS_SILLY_HUGE_STITCHER_H
+
+class silly_huge_stitcher
+{
+  public:
+    silly_huge_stitcher();
+    ~silly_huge_stitcher();
+
+  private:
+};
+
+#endif  // SILLY_UTILS_SILLY_HUGE_STITCHER_H
