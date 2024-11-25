945e9a378fa501e73f8ab8c42fe2c238470baa35
DouLion <734546831@qq.com>
2024-11-25 09:51:14 +0800

    添加python脚本自动创建使用silly_utils的项目

---
+++ tools/python/auto_gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights reserved.
+            北京天智祥信息科技有限公司版权所有
+@website: http://www.tianzhixiang.com.cn/
+@author: dou li yang
+@date: 2024-11-20
+@file: auto_gen.py
+@description: 自动生成带sutil的cmake项目
+@version: v1.0.1 2024-11-20 dou li yang
+"""
+import logger
+
+
+
+if __name__ == '__main__':
+
+    try:
+
+    except Exception as e:
+        print(e)
+
+    exit(0)
