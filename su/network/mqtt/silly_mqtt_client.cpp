300fbc6e1dff542e83c4f4bc5cf80279b90616cd
DouLion <734546831@qq.com>
2024-12-16 18:37:47 +0800

    添加mqtt客户端功能

--- su/network/mqtt/silly_mqtt_client.cpp
+++ su/network/mqtt/silly_mqtt_client.cpp
@@ -9,3 +9,107 @@
  * @version: v1.0.1 2024-09-11 dou li yang
  */
 #include "silly_mqtt_client.h"
+#include <mqtt/client.h>
+#include <mqtt/ssl_options.h>
+
+const auto PERIOD = std::chrono::seconds(20);
+
+const int MAX_BUFFERED_MSGS = 120;  // 120 * 5sec => 10min off-line buffering
+
+bool silly_mqtt_client::publish(const std::string& topic, const std::string& payload)
+{
+    bool status = false;
+    std::string uri = m_protocol + "://" + m_host + ":" + std::to_string(m_port);
+    
+
+    auto conn_opts = mqtt::connect_options_builder()
+        .keep_alive_interval(PERIOD)
+        .clean_session(true)
+        .automatic_reconnect(true)
+        .finalize();
+    conn_opts.set_ssl(mqtt::ssl_options());
+    conn_opts.set_mqtt_version(MQTTVERSION_3_1);
+    conn_opts.set_user_name(m_user);
+    conn_opts.set_password(m_password);
+    mqtt::async_client cli(uri, m_client_id);
+    // mqtt::topic top(cli, topic, m_qos, true);
+    try
+    {
+        cli.connect(conn_opts)->wait();
+        auto msg = mqtt::make_message(topic, payload, m_qos, false);
+        status = cli.publish(msg)->wait_for(20000);  // 毫秒
+        cli.disconnect()->wait();
+    }
+    catch (const mqtt::exception& exc)
+    {
+        SLOG_ERROR("MQTT发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", payload, topic, exc.what());
+    }
+    catch (const std::exception& exc)
+    {
+        SLOG_ERROR("MQTT发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", payload, topic, exc.what());
+    }
+
+    return false;
+}
+
+bool silly_mqtt_client::subscribe(const std::string& topic)
+{
+    return false;
+}
+
+bool silly_mqtt_client::check()
+{
+    return false;
+}
+
+void silly_mqtt_client::client_id(const std::string& ci)
+{
+    m_client_id = ci;
+}
+
+void silly_mqtt_client::user(const std::string& u)
+{
+    m_user = u;
+}
+
+void silly_mqtt_client::password(const std::string& p)
+{
+    m_password = p;
+}
+
+void silly_mqtt_client::host(const std::string& h)
+{
+    m_host = h;
+}
+
+void silly_mqtt_client::port(const int& p)
+{
+    m_port = p;
+}
+
+void silly_mqtt_client::qos(const int& q)
+{
+    m_qos = q;
+}
+
+void silly_mqtt_client::protocol(const std::string& p)
+{
+    m_protocol = p;
+}
+
+void silly_mqtt_client::server(const std::string& s)
+{
+    auto pos = s.find("://");
+    if (pos != std::string::npos)
+    {
+        protocol(s.substr(0, pos));
+    }
+    std::string ss = s.substr(pos + 3);
+    pos = ss.find(":");
+    if (pos != std::string::npos)
+    {
+        port(std::stoi(ss.substr(pos + 1)));
+        ss = ss.substr(0, pos);
+    }
+    host(ss);
+}
--- su/network/mqtt/silly_mqtt_client.h
+++ su/network/mqtt/silly_mqtt_client.h
@@ -5,19 +5,59 @@
  * @author: dou li yang
  * @date: 2024-09-11
  * @file: silly_mqtt_client.h
- * @description: silly_mqtt_client 类声明
+ * @description: mqtt 客户端
  * @version: v1.0.1 2024-09-11 dou li yang
  */
 #ifndef SILLY_UTILS_SILLY_MQTT_CLIENT_H
 #define SILLY_UTILS_SILLY_MQTT_CLIENT_H
+#include <su_marco.h>
+/* 支持的协议
+"mqtt://<host>:<port>"   - TCP, unsecure
+"tcp://<host>:<port>"    (same)
 
+"mqtts://<host>:<port>"  - SSL/TLS
+"ssl://<host>:<port>"     (same)
+
+"ws://<host>:<port>"    - Unsecure websockets
+"wss://<host>:<port>"   - Secure websockets
+ */
 class silly_mqtt_client
 {
   public:
-    silly_mqtt_client();
-    ~silly_mqtt_client();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="payload"></param>
+    /// <returns></returns>
+    bool publish(const std::string& topic, const std::string& payload);
+
+
+    bool subscribe(const std::string& topic);
+
+    bool check();
+
+    void client_id(const std::string& ci);
+
+    void user(const std::string& u);
+    void password(const std::string& p);
+    void host(const std::string& h);
+    void port(const int& p);
+    void qos(const int& q);
+    void protocol(const std::string& p);
+
+    void server(const std::string& s);
+
+
 
   private:
+     std::string m_client_id;
+    std::string m_user;
+     std::string m_protocol{"mqtt"};
+    std::string m_password;
+    std::string m_host;
+    int m_port{1883}; // 1883 默认非加密端口  8883 默认加密端口
+    int m_qos{2}; // 0 最多一次  1 最少一次  2 只有一次
 };
 
 #endif  // SILLY_UTILS_SILLY_MQTT_CLIENT_H
