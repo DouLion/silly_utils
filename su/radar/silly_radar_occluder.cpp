5918cd0deaaa13c44687967cf7857ae90d173f12
Douliyang <734546831@qq.com>
2025-02-24 19:19:30 +0800

    雷达遮挡角实现部分,后面需要补充完整

--- su/radar/silly_radar_occluder.cpp
+++ su/radar/silly_radar_occluder.cpp
@@ -9,10 +9,35 @@
  * @version: v1.0.1 2025-02-14 dou li yang
  */
 #include "silly_radar_occluder.h"
-silly_poly silly_radar_occluder::goccluder_poly(const silly_point& loc, const double& height, const double& deg, const double& r, const double& recr)
+
+silly_point polar2cartesian(const silly_point& center, const double& r, const double& deg)
 {
-    // TODO: 补充实现
-    // 1. 以loc为中心, 从DEM上裁剪出以2*recr为边长的正方向范围的高程,以墨卡托坐标作为实际依据
-    // 2. 
-    return silly_poly();
+    return silly_point(center.x + r * std::cos(DEG2RAD(deg)), center.y + r * std::sin(DEG2RAD(deg)));
+}
+
+bool silly_radar_occluder::dem2polar(const std::string& dem, const silly_point& center, const double& radius)
+{
+    return false;
+}
+
+silly_poly silly_radar_occluder::occluder_poly(const double& height, const double& deg, const double& radius)
+{
+    silly_poly poly;
+    double n = radius * 1000.0 / m_polar_dem.front().size();
+    double dp = 2 * SU_PI / m_polar_dem.size(); // 极坐标角度变化值
+    double rad = DEG2RAD(deg);
+    for (int i = 0; i < m_polar_dem.size(); ++i)
+    {
+        for (int j = 0; j < m_polar_dem[i].size(); ++j)
+        {
+            double h = n * j * std::atan(rad) + height;
+            if (h < m_polar_dem[i][j])
+            {
+                poly.outer_ring.points.push_back(polar2cartesian(m_center, j*n, i * dp));
+            }
+        }
+    }
+
+    return poly;
+
 }
--- su/radar/silly_radar_occluder.h
+++ su/radar/silly_radar_occluder.h
@@ -14,19 +14,26 @@
 class silly_radar_occluder
 {
   public:
+    /// <summary>
+    /// 网格dem转极坐标
+    /// </summary>
+    /// <param name="dem"></param>
+    /// <param name="center">中心坐标</param>
+    /// <param name="radius">采样半径</param>
+    /// <returns></returns>
+    bool dem2polar(const std::string& dem, const silly_point& center, const double& radius);
 
     /// <summary>
     /// 提取雷达遮挡范围矢量
     /// </summary>
-    /// <param name="loc">雷达坐标经纬度</param>
     /// <param name="height">雷达高程</param>
     /// <param name="deg">俯仰角度(度),指向天空为正,指向地面为负</param>
-    /// <param name="r">雷达扫描半径(km)</param>
-    /// <param name="r">正方形范围1/2长(km)</param>
-    silly_poly goccluder_poly(const silly_point& loc, const double& height, const double& deg, const double& r, const double& recr);
+    /// <param name="radius">雷达工作扫描半径(km)</param>
+    silly_poly occluder_poly(const double& height, const double& deg, const double& radius);
 
   private:
-
+    std::vector<std::vector<double>> m_polar_dem;  // 极坐标DEM
+    silly_point m_center;
 };
 
 #endif  // SILLY_UTILS_SILLY_RADAR_OCCLUDER_H
