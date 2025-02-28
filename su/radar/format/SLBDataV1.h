963f601b1ccec43daa884726f2ea4374161a01ad
Douliyang <734546831@qq.com>
2025-02-28 18:44:59 +0800

    水利部雷达格式,为后面数据上报你做准备

---
+++ su/radar/format/SLBDataV1.h
@@ -0,0 +1,23 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2025-02-28
+ * @file: SLBDataV1.h
+ * @description: 水利部雷达格式1.0
+ * @version: v1.0.1 2025-02-28 dou li yang
+ */
+#ifndef SILLY_UTILS_SLB_DATA_V1_H
+#define SILLY_UTILS_SLB_DATA_V1_H
+
+class SLBDataV1
+{
+  public:
+    SLBDataV1();
+    ~SLBDataV1();
+
+  private:
+};
+
+#endif  // SILLY_UTILS_SLB_DATA_V1_H
