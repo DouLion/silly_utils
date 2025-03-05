87f7be40f49e71daf2c95ed4eb2a4284804a365a
Douliyang <734546831@qq.com>
2025-03-05 10:20:36 +0800

    准备读取湖南雷达数据

---
+++ su/radar/format/HunanData.cpp
@@ -0,0 +1,11 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2025-03-05
+ * @file: HunanData.cpp
+ * @description: HunanData实现
+ * @version: v1.0.1 2025-03-05 dou li yang
+ */
+#include "HunanData.h"
---
+++ su/radar/format/HunanData.h
@@ -0,0 +1,23 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2025-03-05
+ * @file: HunanData.h
+ * @description: HunanData 类声明
+ * @version: v1.0.1 2025-03-05 dou li yang
+ */
+#ifndef SILLY_UTILS_HUNANDATA_H
+#define SILLY_UTILS_HUNANDATA_H
+
+class HunanData
+{
+  public:
+    HunanData();
+    ~HunanData();
+
+  private:
+};
+
+#endif  // SILLY_UTILS_HUNANDATA_H
