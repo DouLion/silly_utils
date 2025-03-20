57ec1ff3c4a5c9d513526edca4c0ff94bda09e89
Douliyang <734546831@qq.com>
2025-03-20 09:37:26 +0800

    提高效率

--- extend/ascii_grid_image/ascii_grid_image.cpp
+++ extend/ascii_grid_image/ascii_grid_image.cpp
@@ -6,24 +6,76 @@
 #include <graphics/silly_png.h>
 #include <files/silly_file.h>
 #include <geo/proj/silly_proj.h>
+#include <datetime/silly_timer.h>
 using namespace silly::geo;
 
 #define DEPTH_TINY 1e-2
 
 thread_local void* png_ptr = nullptr;
+thread_local size_t* png_index = nullptr;
+void create_map(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid)
+{
+    if (!png_index)
+    {
+        double mct_left = 0, mct_right = 0, mct_top = 0, mct_bottom = 0;
+        const double gauss_left = xllcorner, gauss_right = xllcorner + cellsize * (ncols - 2), gauss_top = yllcorner + cellsize * (nrows - 2), gauss_bottom = yllcorner;
+        double left_top_x, left_top_y;
+        proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_top, left_top_x, left_top_y);
+        double right_bottom_x, right_bottom_y;
+        proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_bottom, right_bottom_x, right_bottom_y);
+        double left_bottom_x, left_bottom_y;
+        proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_bottom, left_bottom_x, left_bottom_y);
+        double right_top_x, right_top_y;
+        proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_top, right_top_x, right_top_y);
+        double geo_left = SU_MAX(left_top_x, left_bottom_x);
+        double geo_right = SU_MIN(right_bottom_x, right_top_x);
+        double geo_top = SU_MIN(right_top_y, left_top_y);
+        double geo_bottom = SU_MAX(left_bottom_y, right_bottom_y);
 
-void convert_image(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid, double* data, double* qx, double* qy, char* img_path, bool verbose)
+        proj::convert::lonlat_to_mercator(geo_left, geo_top, mct_left, mct_top);
+        proj::convert::lonlat_to_mercator(geo_right, geo_bottom, mct_right, mct_bottom);
+
+        double mct_x_step = (mct_right - mct_left) / (ncols - 2);
+        double mct_y_step = (mct_top - mct_bottom) / (nrows - 2);
+        png_index = new size_t[nrows * ncols];
+        memset(png_index, 0, sizeof(size_t) * nrows * ncols);
+        for (size_t r = 0; r < nrows - 2; ++r)
+        {
+            for (size_t c = 0; c < ncols - 2; ++c)
+            {
+                double tmp_mct_x = mct_left + c * mct_x_step;
+                double tmp_mct_y = mct_top - r * mct_y_step;
+                double tmp_gauss_x, tmp_gauss_y;
+                proj::convert::mercator_to_gauss(mid, tmp_mct_x, tmp_mct_y, tmp_gauss_x, tmp_gauss_y);
+
+                int gcol = static_cast<size_t>(std::round((tmp_gauss_x - gauss_left) / cellsize + 1));
+                int grol = static_cast<size_t>(std::round((gauss_top - tmp_gauss_y) / cellsize));
+                if (gcol >= 0 && grol > 0 && gcol < ncols && grol < nrows)
+                {
+                    size_t dpos = SU_MAX(0, SU_MIN(grol * ncols + gcol, nrows * ncols));
+                    size_t spos = r * ncols + c;
+                    png_index[spos] = static_cast<size_t>(dpos);
+                }
+            }
+        }
+    }
+}
+
+void convert_image(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid, double* Hdata, double* qx, double* qy, char* img_path, bool verbose)
 {
     double uMax = -99999.0, uMin = 99999.0, vMax = -99999.0, vMin = 99999.0;
     double hMax = -99999.0, hMin = 99999.0;
-    if (data)
+    if (!Hdata)
+    {
+        return;
+    }
     {
         for (size_t r = 0; r < nrows; ++r)
         {
             for (size_t c = 0; c < ncols; ++c)
             {
                 int pos = SU_MAX(0, SU_MIN(r * ncols + c, nrows * ncols));
-                double h = data[pos];
+                double h = Hdata[pos];
                 hMax = SU_MAX(hMax, h);
                 hMin = SU_MIN(hMin, h);
                 if (h < DEPTH_TINY)
@@ -48,63 +100,31 @@ void convert_image(double ncols, double nrows, double xllcorner, double yllcorne
         }
     }
 
-    double mct_left = 0, mct_right = 0, mct_top = 0, mct_bottom = 0;
-    const double gauss_left = xllcorner, gauss_right = xllcorner + cellsize * (ncols - 2), gauss_top = yllcorner + cellsize * (nrows - 2), gauss_bottom = yllcorner;
-    double left_top_x, left_top_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_top, left_top_x, left_top_y);
-    double right_bottom_x, right_bottom_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_bottom, right_bottom_x, right_bottom_y);
-    double left_bottom_x, left_bottom_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_bottom, left_bottom_x, left_bottom_y);
-    double right_top_x, right_top_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_top, right_top_x, right_top_y);
-    double geo_left = SU_MAX(left_top_x, left_bottom_x);
-    double geo_right = SU_MIN(right_bottom_x, right_top_x);
-    double geo_top = SU_MIN(right_top_y, left_top_y);
-    double geo_bottom = SU_MAX(left_bottom_y, right_bottom_y);
-
-    proj::convert::lonlat_to_mercator(geo_left, geo_top, mct_left, mct_top);
-    proj::convert::lonlat_to_mercator(geo_right, geo_bottom, mct_right, mct_bottom);
-
-    double mct_x_step = (mct_right - mct_left) / (ncols - 2);
-    double mct_y_step = (mct_top - mct_bottom) / (nrows - 2);
     silly::png::data pd, pd2;
     pd.create((ncols - 2), (nrows - 2), silly::color::eptRGB);
     pd2.create((ncols - 2), (nrows - 2), silly::color::eptRGB);
     double hstep = (hMax - hMin) / 255;
     double ustep = (uMax - uMin) / 255;
     double vstep = (vMax - vMin) / 255;
-    if (verbose)
-    {
-        SU_INFO_PRINT("mid: %.2f", mid)
-        SU_INFO_PRINT("Width: %d, Heigth: %d", ncols - 2, nrows - 2)
-        SU_INFO_PRINT("LonLat:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", geo_left, geo_top, geo_right, geo_bottom)
-        SU_INFO_PRINT("Mct:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", mct_left, mct_top, mct_right, mct_bottom)
-        std::cout << "H: " << hMax << " " << hMin << std::endl;
-        std::cout << "U: " << uMax << " " << uMin << std::endl;
-        std::cout << "V: " << vMax << " " << vMin << std::endl;
-        std::cout << "Step: " << hstep << " " << ustep << " " << vstep << std::endl;
-    }
+    size_t* pi = png_index;
+
     for (size_t r = 0; r < nrows - 2; ++r)
     {
         for (size_t c = 0; c < ncols - 2; ++c)
         {
-            double tmp_mct_x = mct_left + c * mct_x_step;
-            double tmp_mct_y = mct_top - r * mct_y_step;
-            double tmp_gauss_x, tmp_gauss_y;
-            proj::convert::mercator_to_gauss(mid, tmp_mct_x, tmp_mct_y, tmp_gauss_x, tmp_gauss_y);
-
-            int gcol = std::round((tmp_gauss_x - gauss_left) / cellsize + 1);
-            int grol = std::round((gauss_top - tmp_gauss_y) / cellsize);
-            double v = 0., qu = 0., qv = 0.;
-            if (gcol >= 0 && grol > 0 && gcol < ncols && grol < nrows)
+            if (*pi == 0)
             {
-                int pos = SU_MAX(0, SU_MIN(grol * ncols + gcol, nrows * ncols));
-                v = data[pos];
+                pi++;
+                continue;
+            }
+            double v = 0., qu = 0., qv = 0.;
+
+            {
+                v = Hdata[*pi];
                 if (v > DEPTH_TINY)
                 {
-                    qu = qx[pos] / v;
-                    qv = qy[pos] / v;
+                    qu = qx[*pi] / v;
+                    qv = qy[*pi] / v;
                 }
                 else
                 {
@@ -163,6 +183,7 @@ void convert_image(double ncols, double nrows, double xllcorner, double yllcorne
 unsigned char* get_image_data(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid, double* data, int* length, bool verbose)
 {
     *length = 0;
+    silly_timer timer;
     double hMax = -99999.0, hMin = 99999.0;
     if (data)
     {
@@ -181,61 +202,50 @@ unsigned char* get_image_data(double ncols, double nrows, double xllcorner, doub
             }
         }
     }
+    if (verbose)
+    {
+        SU_INFO_PRINT("Filter Max:  %.2f ms", timer.elapsed_ms())
+        timer.restart();
+    }
 
-    double mct_left = 0, mct_right = 0, mct_top = 0, mct_bottom = 0;
-    const double gauss_left = xllcorner, gauss_right = xllcorner + cellsize * (ncols - 2), gauss_top = yllcorner + cellsize * (nrows - 2), gauss_bottom = yllcorner;
-    double left_top_x, left_top_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_top, left_top_x, left_top_y);
-    double right_bottom_x, right_bottom_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_bottom, right_bottom_x, right_bottom_y);
-    double left_bottom_x, left_bottom_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_left, gauss_bottom, left_bottom_x, left_bottom_y);
-    double right_top_x, right_top_y;
-    proj::convert::gauss_to_lonlat(mid, gauss_right, gauss_top, right_top_x, right_top_y);
-    double geo_left = SU_MAX(left_top_x, left_bottom_x);
-    double geo_right = SU_MIN(right_bottom_x, right_top_x);
-    double geo_top = SU_MIN(right_top_y, left_top_y);
-    double geo_bottom = SU_MAX(left_bottom_y, right_bottom_y);
-
-    proj::convert::lonlat_to_mercator(geo_left, geo_top, mct_left, mct_top);
-    proj::convert::lonlat_to_mercator(geo_right, geo_bottom, mct_right, mct_bottom);
-
-    double mct_x_step = (mct_right - mct_left) / (ncols - 2);
-    double mct_y_step = (mct_top - mct_bottom) / (nrows - 2);
     silly::png::data pd;
     pd.create((ncols - 2), (nrows - 2), silly::color::eptRGB);
     double hstep = (hMax - hMin) / 255;
-    if (verbose)
-    {
-        SU_INFO_PRINT("mid: %.2f", mid)
-        SU_INFO_PRINT("LonLat:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", geo_left, geo_top, geo_right, geo_bottom)
-        SU_INFO_PRINT("Mct:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", mct_left, mct_top, mct_right, mct_bottom)
-        SU_INFO_PRINT("Depth max:%.6f, min:%6f", hMax, hMin)
-        SU_INFO_PRINT("Step:%.6f", hstep)
-    }
+    /* if (verbose)
+     {
+         SU_INFO_PRINT("mid: %.2f", mid)
+         SU_INFO_PRINT("LonLat:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", geo_left, geo_top, geo_right, geo_bottom)
+         SU_INFO_PRINT("Mct:\n\tleft: %.6f, top: %.6f, right: %.6f, bottom: %.6f", mct_left, mct_top, mct_right, mct_bottom)
+         SU_INFO_PRINT("Depth max:%.6f, min:%6f", hMax, hMin)
+         SU_INFO_PRINT("Step:%.6f", hstep)
+     }*/
+    timer.restart();
+    size_t* pi = png_index;
     for (size_t r = 0; r < nrows - 2; ++r)
     {
         for (size_t c = 0; c < ncols - 2; ++c)
         {
-            double tmp_mct_x = mct_left + c * mct_x_step;
-            double tmp_mct_y = mct_top - r * mct_y_step;
-            double tmp_gauss_x, tmp_gauss_y;
-            proj::convert::mercator_to_gauss(mid, tmp_mct_x, tmp_mct_y, tmp_gauss_x, tmp_gauss_y);
-
-            int gcol = std::round((tmp_gauss_x - gauss_left) / cellsize + 1);
-            int grol = std::round((gauss_top - tmp_gauss_y) / cellsize);
-            double v = 0., qu = 0., qv = 0.;
-            if (gcol >= 0 && grol > 0 && gcol < ncols && grol < nrows)
+            if (*pi == 0)
             {
-                int pos = SU_MAX(0, SU_MIN(grol * ncols + gcol, nrows * ncols));
-                v = data[pos];
+                pi++;
+                continue;
             }
+            double v = 0.;
+            v = data[*pi];
+            pi++;
             silly::color cH;
             // 写入深度数据
             cH.red = static_cast<unsigned char>(std::min(255., (v - hMin) / hstep));
             pd.pixel(r, c, cH);
         }
+        pi++;
+        pi++;
     }
+    if (verbose)
+    {
+        SU_INFO_PRINT("CorrdConvert: %.2f ms", timer.elapsed_ms())
+    }
+    timer.restart();
     std::string h_pd_data;
     {
         h_pd_data.resize(24);
@@ -267,6 +277,10 @@ unsigned char* get_image_data(double ncols, double nrows, double xllcorner, doub
         }
     }
     pd.release();
+    if (verbose)
+    {
+        SU_INFO_PRINT("PngConvert: %.2f ms", timer.elapsed_ms())
+    }
     return (unsigned char*)png_ptr;
 }
 void free_image_data()
@@ -275,5 +289,14 @@ void free_image_data()
     {
         free(png_ptr);
         png_ptr = nullptr;
+    }
+}
+
+void free_map()
+{
+    if (png_index)
+    {
+        delete[] png_index;
+        png_index = nullptr;
     }
 }
--- extend/ascii_grid_image/ascii_grid_image.h
+++ extend/ascii_grid_image/ascii_grid_image.h
@@ -22,6 +22,8 @@ extern "C" {
     TZX_DLL_EXPORT void convert_image(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid, double* data, double* qx, double* qy, char* img_path, bool verbose);
     TZX_DLL_EXPORT unsigned char* get_image_data(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid, double* data, int* length, bool verbose);
     TZX_DLL_EXPORT void free_image_data();
+    TZX_DLL_EXPORT void create_map(double ncols, double nrows, double xllcorner, double yllcorner, double cellsize, double mid);
+    TZX_DLL_EXPORT void free_map();
 
 #ifdef __cplusplus
 }
