634562d727feb68e655124f89779b437be980187
DouLion <734546831@qq.com>
2025-07-11 12:01:54 +0800

    mqtt发布测试通过

--- su/network/mqtt/silly_mqtt_client.cpp
+++ su/network/mqtt/silly_mqtt_client.cpp
@@ -21,18 +21,29 @@ const int MAX_BUFFERED_MSGS = 120;  // 120 * 5sec => 10min off-line buffering
 bool silly_mqtt_client::publish(const std::string& topic, const std::string& payload)
 {
     bool status = false;
-    std::string uri = m_protocol + "://" + m_host + ":" + std::to_string(m_port);
+    std::string uri;
+    if ("mqtt" == m_protocol)
+    {
+        uri = m_host + ":" + std::to_string(m_port);
+    }
+    else
+    {
+        uri = m_protocol + "://" + m_host + ":" + std::to_string(m_port);
+    }
 #if ENABLE_PAHO_MQTT
 
-    auto conn_opts = mqtt::connect_options_builder().keep_alive_interval(PERIOD).clean_session(true).automatic_reconnect(true).finalize();
+    auto connBuilder = mqtt::connect_options_builder();
+    // auto conn_opts = mqtt::connect_options_builder().keep_alive_interval(PERIOD).clean_session(true).automatic_reconnect(true).finalize();
+    auto conn_opts = connBuilder.keep_alive_interval(std::chrono::seconds(30)).finalize();
     conn_opts.set_ssl(mqtt::ssl_options());
     conn_opts.set_mqtt_version(MQTTVERSION_3_1);
-    conn_opts.set_user_name(m_user);
-    conn_opts.set_password(m_password);
-    mqtt::async_client cli(uri, m_client_id);
-    // mqtt::topic top(cli, topic, m_qos, true);
+
+    // conn_opts.set_user_name(m_user);
+    //  conn_opts.set_password(m_password);
+
     try
     {
+        mqtt::async_client cli(uri, m_client_id);
         cli.connect(conn_opts)->wait();
         auto msg = mqtt::make_message(topic, payload, m_qos, false);
         status = cli.publish(msg)->wait_for(20000);  // 毫秒
@@ -40,14 +51,14 @@ bool silly_mqtt_client::publish(const std::string& topic, const std::string& pay
     }
     catch (const mqtt::exception& exc)
     {
-        SLOG_ERROR("MQTT发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", payload, topic, exc.what());
+        SLOG_ERROR("MQTT({})发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", uri, payload, topic, exc.what());
     }
     catch (const std::exception& exc)
     {
-        SLOG_ERROR("MQTT发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", payload, topic, exc.what());
+        SLOG_ERROR("MQTT({})发布内容 :\n{} \n到主题:[ {} ]失败,错误信息:\n {}.", uri, payload, topic, exc.what());
     }
 #endif
-    return false;
+    return status;
 }
 
 bool silly_mqtt_client::subscribe(const std::string& topic)
