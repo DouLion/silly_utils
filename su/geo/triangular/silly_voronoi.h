6d3dafc697cbaba17b8b949c4455b262fef4c5b9
douliyang <734546831@qq.com>
2025-08-21 12:05:24 +0800

    准备处理voronoi

---
+++ su/geo/triangular/silly_voronoi.h
@@ -0,0 +1,17 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2025-08-13
+ * @file: silly_voronoi.cpp
+ * @description: silly_voronoi 头文件
+ * @version: v1.0.1 2025-08-13 dou li yang
+ */
+#ifndef SILLY_VORONOI_H
+#define SILLY_VORONOI_H
+
+
+
+
+#endif //SILLY_VORONOI_H
