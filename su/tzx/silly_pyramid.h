5ed839311118828d3456867e8d13d9d225658388
douliyang <734546831@qq.com>
2025-12-09 19:21:23 +0800

    添加silly_pyramid,简化include

---
+++ su/tzx/silly_pyramid.h
@@ -0,0 +1,21 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2025-12-09
+ * @file: silly_pyramid.cpp
+ * @description: silly_pyramid 头文件
+ * @version: v1.0.1 2025-12-09 dou li yang
+ */
+
+#pragma once
+
+#include <tzx/pyramid/silly_pyramid_marco.h>
+#include <tzx/pyramid/silly_pyramid_base.h>
+#include <tzx/pyramid/silly_pyramid_block.h>
+#include <tzx/pyramid/silly_pyramid_data.h>
+#include <tzx/pyramid/silly_pyramid_info.h>
+#include <tzx/pyramid/silly_pyramid_index.h>
+#include <tzx/pyramid/silly_pyramid_full_text.h>
+#include <tzx/pyramid/silly_pyramid_handle.h>
