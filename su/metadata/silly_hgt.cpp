42a91bb1337dc8326a9d128bc2c4e549862b5193
douliyang <734546831@qq.com>
2026-04-01 11:51:33 +0800

    准备补充HTG的高程文件读取

---
+++ su/metadata/silly_hgt.cpp
@@ -0,0 +1,11 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2026-04-01
+ * @file: silly_hgt
+ * @description: silly_hgt实现
+ * @version: v1.0.1 2026-04-01 dou li yang
+ */
+#include "silly_hgt.h"
---
+++ su/metadata/silly_hgt.h
@@ -0,0 +1,18 @@
+/*
+ * @copyright: Beijing TianZhiXiang Information Technology Co., Ltd. All rights
+ * reserved. 北京天智祥信息科技有限公司版权所有
+ * @website: http://www.tianzhixiang.com.cn/
+ * @author: dou li yang
+ * @date: 2026-04-01
+ * @file: silly_hgt.h
+ * @description: raw 高程栅格数据
+ * @version: v1.0.1 2026-04-01 dou li yang
+ */
+#ifndef SILLY_HGT_H
+#define SILLY_HGT_H
+
+class suHGT
+{
+};
+
+#endif //SILLY_HGT_H
